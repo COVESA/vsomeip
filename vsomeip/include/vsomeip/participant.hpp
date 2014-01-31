b90d2663b5b29b50a14a3d1d2086cb32cfc38c4a
Lutz Bichler <Lutz.Bichler@bmw.de>
2014-01-31 15:58:18 +0100

    Moves common send-method from client-/service-interface to common base interface participant.hpp. The specific send-functions for messages that are already serialized remain in the specific interfaces as they differ by the needed argument set (service interface needs additional information about the send target).

--- vsomeip/include/vsomeip/participant.hpp
+++ vsomeip/include/vsomeip/participant.hpp
@@ -69,6 +69,21 @@ public:
 								   service_id _service_id,
 								   method_id _method_id) = 0;
 
+    /// Send a Some/IP message using the message structure provided by
+	/// vsomeip. A call to send immediately returns and the message is sent
+	/// asynchronously. The point in time the message is sent also depends on
+	/// the setting for the parameter _flush. If _flush is set to false, the
+	/// message is queued and sent together with other messages in a single
+	/// data packet as soon as a call to send with parameter _flush set to true
+	/// follows or the maximum data packet size is exceeded or no flushing was
+	/// done for at least VSOMEIP_FLUSH_TIMEOUT milliseconds.
+	/// \param _message Pointer to the message object that shall be sent.
+	/// \param _flush Flag that must be set to true (default) if the the message
+	/// shall be sent directly or to false if the packetizer shall be used.
+	/// \warning This method is not thread safe! If you want to call it on the same object
+	/// from different threads, you need to synchronize the calls.
+	virtual bool send(const message_base *_message, bool _flush = true) = 0;
+
 	/// Consume one queued event. This e.g. triggers one send or receive operation
 	/// to be actually executed.
 	/// \warning Must be called from a single thread.
--- vsomeip/include/vsomeip/service.hpp
+++ vsomeip/include/vsomeip/service.hpp
@@ -24,26 +24,12 @@ class endpoint;
 /// instances can be created by a call to factory::create_service.
 class service
 		: virtual public participant {
-public:
-	/// Send a Some/IP message using the message structure provided by
-	/// vsomeip. A call to send immediately returns and the message is sent
-	/// asynchronously. The point in time the message is sent also depends on
-	/// the setting for the parameter _flush. If _flush is set to false, the
-	/// message is queued and sent together with other messages in a single
-	/// data packet as soon as a call to send with parameter _flush set to true
-	/// follows or the maximum data packet size is exceeded or no flushing was
-	/// done for at least VSOMEIP_FLUSH_TIMEOUT milliseconds.
-	/// \param _message Pointer to the message object that shall be sent.
-	/// \param _flush Flag that must be set to true (default) if the the message
-	/// shall be sent directly or to false if the packetizer shall be used.
-	/// \warning This method is not thread safe! If you want to call it on the same object
-	/// from different threads, you need to synchronize the calls.
-	virtual bool send(const message_base *_message, bool _flush = true) = 0;
-    
+public:    
     /// Allows to send an already serialized Some/IP message. The behavior is
 	/// equal to sending a Some/IP message using the provided message structure.
 	/// \param _data Pointer to the serialized message data.
 	/// \param _size Amount of data that shall be sent.
+    /// \param _endpoint target endpoint the message shall be sent to
 	/// \param _flush Flag that must be set to true (default) if the the message
 	/// shall be sent directly or to false if the packetizer shall be used.
 	/// \warning This method is not thread safe! If you want to call it on the same object
