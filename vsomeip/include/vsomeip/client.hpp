97479c50d8cb9f25a04893dabdf220caf4e15fd7
Lutz Bichler <Lutz.Bichler@bmw.de>
2014-01-31 16:03:53 +0100

    Reverted last commit as it seems that the specific send function cannot be resolved anymore.

--- vsomeip/include/vsomeip/client.hpp
+++ vsomeip/include/vsomeip/client.hpp
@@ -23,18 +23,31 @@ namespace vsomeip {
 class client
 		: virtual public participant {
 public:
+	/// Send a Some/IP message using the message structure provided by
+	/// vsomeip. A call to send immediately returns and the message is sent
+	/// asynchronously. The point in time the message is sent also depends on
+	/// the setting for the parameter _flush. If _flush is set to false, the
+	/// message is queued and sent together with other messages in a single
+	/// data packet as soon as a call to send with parameter _flush set to true
+	/// follows or the maximum data packet size is exceeded or no flushing was
+	/// done for at least VSOMEIP_FLUSH_TIMEOUT milliseconds.
+	/// \param _message Pointer to the message object that shall be sent.
+	/// \param _flush Flag that must be set to true (default) if the the message
+	/// shall be sent directly or to false if the packetizer shall be used.
+	/// \warning This method is not thread safe! If you want to call it on the same object
+	/// from different threads, you need to synchronize the calls.
+	virtual bool send(const message_base *_message, bool _flush = true) = 0;
+	
     /// Send an already serialized Some/IP message. The behavior is
 	/// equal to sending a Some/IP message using the provided message structure.
 	/// \param _data Pointer to the serialized message data.
 	/// \param _size Amount of data that shall be sent.
-	/// \param _flush Flag that must be set to true (default) if the the 
-    /// message shall be sent directly or to false if the packetizer shall be 
-    /// used.
-	/// \warning This method is not thread safe! If you want to call it on the 
-    /// same object from different threads, you need to synchronize the calls.
-	virtual bool send(const uint8_t *_data, 
-                      uint32_t _size, 
-                      bool _flush = true) = 0;
+	/// \param _flush Flag that must be set to true (default) if the the message
+	/// shall be sent directly or to false if the packetizer shall be used.
+	/// \warning This method is not thread safe! If you want to call it on the same object
+	/// from different threads, you need to synchronize the calls.
+	virtual bool send(const uint8_t *_data, uint32_t _size,
+						bool _flush = true) = 0;
 };
 
 } // namespace vsomeip
