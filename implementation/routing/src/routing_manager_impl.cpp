4d22dfa1f8c255b9b2b43fa8c189e3ab504c9c5e
Evgenii Demin <EDemin@luxoft.com>
2018-07-23 12:26:04 +0300

    Generalize e2e profiles creation.
    cfg::e2e structure contained:
    * general e2e profile prameters
    * strictly typed profile01 specific parameters.
    Now this structure contains only general parameters and a dictionary containing string values for all other  perameters.
    It allows developer to easily extend set of used e2e-profiles.
    rountin_manager intialization code was also rework to adopt configuration changes.

--- implementation/routing/src/routing_manager_impl.cpp
+++ implementation/routing/src/routing_manager_impl.cpp
@@ -63,6 +63,100 @@
 #include "../../e2e_protection/include/e2e/profile/profile_custom/protector.hpp"
 #include "../../e2e_protection/include/e2e/profile/profile_custom/checker.hpp"
 
+namespace {
+
+template<typename value_t>
+value_t read_value_from_config(std::shared_ptr<vsomeip::cfg::e2e> config,
+                               const std::string& name,
+                               value_t default_value = value_t()) {
+    if(config && config->custom_parameters.count(name)) {
+        value_t value;
+        std::stringstream its_converter;
+        its_converter << config->custom_parameters[name];
+        its_converter >> value;
+        return value;
+    }
+
+    return default_value;
+}
+
+bool is_e2e_profile_supported(const std::string& name) {
+    return name == "CRC8" || name == "CRC32";
+}
+
+template<typename config_t>
+config_t make_e2e_profile_config(std::shared_ptr<vsomeip::cfg::e2e>) {
+    return config_t();
+}
+
+template<>
+vsomeip::e2e::profile01::profile_config
+make_e2e_profile_config(std::shared_ptr<vsomeip::cfg::e2e> config) {
+    uint16_t crc_offset = read_value_from_config<uint16_t>(config, "crc_offset");
+    uint16_t data_length = read_value_from_config<uint16_t>(config, "data_length");
+
+    // counter field behind CRC8
+    uint16_t counter_offset = read_value_from_config<uint16_t>(config, "counter_offset", 8);
+
+    // data id nibble behind 4 bit counter value
+    uint16_t data_id_nibble_offset = read_value_from_config<uint16_t>(config, "data_id_nibble_offset", 12);
+
+    vsomeip::e2e::profile01::p01_data_id_mode data_id_mode =
+        static_cast<vsomeip::e2e::profile01::p01_data_id_mode>(
+            read_value_from_config<uint8_t>(config, "data_id_mode"));
+
+    return vsomeip::e2e::profile01::profile_config(crc_offset, config->data_id, data_id_mode,
+        data_length, counter_offset, data_id_nibble_offset);
+}
+
+template<>
+vsomeip::e2e::profile_custom::profile_config
+make_e2e_profile_config(std::shared_ptr<vsomeip::cfg::e2e> config) {
+    uint16_t crc_offset = read_value_from_config<uint16_t>(config, "crc_offset");
+    return vsomeip::e2e::profile_custom::profile_config(crc_offset);
+}
+
+
+template<typename config_t, typename checker_t, typename protector_t>
+std::tuple<std::shared_ptr<vsomeip::e2e::profile_interface::checker>,
+           std::shared_ptr<vsomeip::e2e::profile_interface::protector>>
+process_e2e_profile(std::shared_ptr<vsomeip::cfg::e2e> config) {
+    config_t profile_config = make_e2e_profile_config<config_t>(config);
+
+    std::shared_ptr<vsomeip::e2e::profile_interface::checker> checker;
+    if ((config->variant == "checker") || (config->variant == "both")) {
+        checker = std::make_shared<checker_t>(profile_config);
+    }
+
+    std::shared_ptr<vsomeip::e2e::profile_interface::protector> protector;
+    if ((config->variant == "protector") || (config->variant == "both")) {
+        protector = std::make_shared<protector_t>(profile_config);
+    }
+
+    return std::make_tuple(checker, protector);
+}
+
+std::tuple<std::shared_ptr<vsomeip::e2e::profile_interface::checker>,
+           std::shared_ptr<vsomeip::e2e::profile_interface::protector>>
+process_e2e_config(std::shared_ptr<vsomeip::cfg::e2e> config) {
+    if(config->profile == "CRC8") {
+        return process_e2e_profile<vsomeip::e2e::profile01::profile_config,
+                                   vsomeip::e2e::profile01::profile_01_checker,
+                                   vsomeip::e2e::profile01::protector>(config);
+    }
+
+    if(config->profile == "CRC32") {
+        return process_e2e_profile<vsomeip::e2e::profile_custom::profile_config,
+                                   vsomeip::e2e::profile_custom::profile_custom_checker,
+                                   vsomeip::e2e::profile_custom::protector>(config);
+    }
+
+    return std::make_tuple(nullptr, nullptr);
+}
+
+
+} // namespace
+
 namespace vsomeip {
 
 routing_manager_impl::routing_manager_impl(routing_manager_host *_host) :
@@ -119,25 +213,19 @@ void routing_manager_impl::init() {
         std::map<e2exf::data_identifier, std::shared_ptr<cfg::e2e>> its_e2e_configuration = configuration_->get_e2e_configuration();
         for (auto &identifier : its_e2e_configuration) {
             auto its_cfg = identifier.second;
-            if(its_cfg->profile == "CRC8") {
+            if(is_e2e_profile_supported(its_cfg->profile)) {
                 e2exf::data_identifier its_data_identifier = {its_cfg->service_id, its_cfg->event_id};
-                e2e::profile01::profile_config its_profile_config = e2e::profile01::profile_config(its_cfg->crc_offset, its_cfg->data_id,
-                    (e2e::profile01::p01_data_id_mode) its_cfg->data_id_mode, its_cfg->data_length, its_cfg->counter_offset, its_cfg->data_id_nibble_offset);
-                if ((its_cfg->variant == "protector") || (its_cfg->variant == "both")) {
-                    custom_protectors[its_data_identifier] = std::make_shared<e2e::profile01::protector>(its_profile_config);
-                }
-                if ((its_cfg->variant == "checker") || (its_cfg->variant == "both")) {
-                    custom_checkers[its_data_identifier] = std::make_shared<e2e::profile01::profile_01_checker>(its_profile_config);
-                }
-            } else if(its_cfg->profile == "CRC32") {
-                e2exf::data_identifier its_data_identifier = {its_cfg->service_id, its_cfg->event_id};
-                e2e::profile_custom::profile_config its_profile_config = e2e::profile_custom::profile_config(its_cfg->crc_offset);
 
-                if ((its_cfg->variant == "protector") || (its_cfg->variant == "both")) {
-                    custom_protectors[its_data_identifier] = std::make_shared<e2e::profile_custom::protector>(its_profile_config);
+                std::shared_ptr<e2e::profile_interface::checker> checker;
+                std::shared_ptr<e2e::profile_interface::protector> protector;
+                std::tie(checker, protector) = process_e2e_config(its_cfg);
+
+                if(checker) {
+                     custom_checkers[its_data_identifier] = checker;
                 }
-                if ((its_cfg->variant == "checker") || (its_cfg->variant == "both")) {
-                    custom_checkers[its_data_identifier]   = std::make_shared<e2e::profile_custom::profile_custom_checker>(its_profile_config);
+
+                if(protector) {
+                     custom_protectors[its_data_identifier] = protector;
                 }
             }
         }
