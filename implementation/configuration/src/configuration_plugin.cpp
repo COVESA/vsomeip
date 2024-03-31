38a464dcd72d89aa2a75458566a01a692ab2615b
gpu <gpu@gpu>
2024-03-31 11:17:34 +0800

    fix dynamic_pointer_cast error

--- implementation/configuration/src/configuration_plugin.cpp
+++ implementation/configuration/src/configuration_plugin.cpp
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "../include/configuration_impl.hpp"
+#include "../include/configuration_plugin.hpp"
 
 namespace vsomeip_v3 {
 namespace cfg {
