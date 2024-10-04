9922c1c2e2f01a4dda962397e855752c529f35bf
Rui Graça <rui.graca@ctw.bmwgroup.com>
2024-10-04 12:23:14 +0100

    Update wireshark dissector
    Summary
    Adds parsing to the routing_info command wireshark dissector
    Details
    The wireshark vsomeip dissector will now also parse the payload
    of the routing_info command entries.
    It will display the information of the subcommands:
    add_client, del_client, add_service and del_service.
    Adds also parsing for the request service and fixes the assign client name.

--- tools/wireshark_plugin/vsomeip-dissector.lua
+++ tools/wireshark_plugin/vsomeip-dissector.lua
@@ -1,6 +1,6 @@
 --[[
+    vsomeip-dissector.lua V0.0.2
     Wireshark Lua vsomeip protocol dissector
-    vsomeip-dissector.lua V0.0.1
 --]]
 
 protocol_name = 'vsomeip'
@@ -10,6 +10,7 @@ vsomeip_command = ProtoField.uint8(protocol_name .. '.command' , "Command", base
 vsomeip_version = ProtoField.uint16(protocol_name .. '.version' , "Version", base.DEC)
 vsomeip_client = ProtoField.uint16(protocol_name .. '.client' , "Client", base.HEX)
 vsomeip_size = ProtoField.uint32(protocol_name .. '.size' , "Size", base.DEC)
+vsomeip_newclient = ProtoField.uint16(protocol_name .. '.newclient' , "Assigned Client", base.HEX)
 
 vsomeip_name = ProtoField.string(protocol_name .. '.name' , "Name")
 
@@ -44,9 +45,6 @@ vsomeip_policy = ProtoField.bytes(protocol_name .. '.policy' , "Policy", base_HE
 -- TODO Parse filter in VSOMEIP_SUBSCRIBE
 vsomeip_filter = ProtoField.bytes(protocol_name .. '.filter' , "Filter", base_HEX)
 
--- TODO make this a subtree to list all entries in VSOMEIP_ROUTING_INFO
-vsomeip_entries = ProtoField.bytes(protocol_name .. '.entries' , "Entries", base_HEX)
-
 -- TODO make this a subtree to list all offered_services in VSOMEIP_OFFERED_SERVICES_RESPONSE
 vsomeip_offered_services = ProtoField.bytes(protocol_name .. '.offered_services' , "OfferedServices", base_HEX)
 
@@ -59,16 +57,52 @@ vsomeip_credentials = ProtoField.bytes(protocol_name .. '.credentials' , "Creden
 -- TODO make this a subtree to list all configurations in VSOMEIP_DISTRIBUTE_SECURITY_POLICIES
 vsomeip_policies = ProtoField.bytes(protocol_name .. '.policies' , "Policies", base_HEX)
 
+-- TODO make this a subtree to list all entries in VSOMEIP_REGISTER_EVENT
+vsomeip_entries = ProtoField.bytes(protocol_name .. '.entries' , "Entries", base_HEX)
+
 vsomeip_protocol.fields = {
     vsomeip_command, vsomeip_version,
     vsomeip_client, vsomeip_size,
-    vsomeip_name,
+    vsomeip_name, vsomeip_newclient,
     vsomeip_instance, vsomeip_reliable, vsomeip_crc, vsomeip_dstClient, vsomeip_payload,
     vsomeip_service, vsomeip_eventgroup, vsomeip_subscriber, vsomeip_notifier, vsomeip_event, vsomeip_id,
     vsomeip_provided,
     vsomeip_major, vsomeip_minor,
     vsomeip_filter,
     vsomeip_entries
+}
+
+vsomeip_routing_info_name = 'vsomeip_routing_info'
+vsomeip_routing_info = Proto(vsomeip_routing_info_name, vsomeip_routing_info_name:upper())
+vsomeip_routing_info_command = ProtoField.uint8(vsomeip_routing_info_name .. '.subcommand' , "SubCommand", base.HEX)
+vsomeip_routing_info_size = ProtoField.uint32(vsomeip_routing_info_name .. '.size' , "Size", base.DEC)
+vsomeip_routing_info_client = ProtoField.uint16(vsomeip_routing_info_name .. '.client' , "Client", base.HEX)
+vsomeip_routing_info_address = ProtoField.ipv4(vsomeip_routing_info_name .. '.address' , "Address", base_HEX)
+vsomeip_routing_info_port = ProtoField.uint16(vsomeip_routing_info_name .. '.port' , "Port", base.DEC)
+vsomeip_routing_info_size_client = ProtoField.uint32(vsomeip_routing_info_name .. '.sizeClient' , "Client Info Size", base.DEC)
+vsomeip_routing_info_size_service = ProtoField.uint32(vsomeip_routing_info_name .. '.sizeService' , "Service Info Size", base.DEC)
+
+vsomeip_routing_info.fields = {
+    vsomeip_routing_info_command,
+    vsomeip_routing_info_size,
+    vsomeip_routing_info_client,
+    vsomeip_routing_info_address,
+    vsomeip_routing_info_port,
+    vsomeip_routing_info_size_client,
+    vsomeip_routing_info_size_service
+}
+
+vsomeip_routing_info_service_name = 'vsomeip_routing_info_service'
+vsomeip_routing_info_service = Proto(vsomeip_routing_info_service_name, vsomeip_routing_info_service_name:upper())
+vsomeip_routing_info_service_id = ProtoField.uint16(vsomeip_routing_info_service_name .. '.serviceId' , "Service", base.HEX)
+vsomeip_routing_info_instance = ProtoField.uint16(vsomeip_routing_info_service_name .. '.instance' , "Instance", base.HEX)
+vsomeip_routing_info_major = ProtoField.uint8(vsomeip_routing_info_service_name .. '.major' , "Major", base.DEC)
+vsomeip_routing_info_minor = ProtoField.uint32(vsomeip_routing_info_service_name .. '.minor' , "Minor", base.DEC)
+vsomeip_routing_info_service.fields = {
+    vsomeip_routing_info_service_id,
+    vsomeip_routing_info_instance,
+    vsomeip_routing_info_major,
+    vsomeip_routing_info_minor
 }
 
 function vsomeip_protocol.dissector(buffer, pinfo, tree)
@@ -107,19 +141,20 @@ function vsomeip_protocol.dissector(buffer, pinfo, tree)
     subtree:add_le(vsomeip_version, command_version)
 
     local buffer_cursor = 7
+    local command_size = {0,0,0,0}
     if command_name ~= "VSOMEIP_SUSPEND" then
         -- Client
         local command_client = buffer(buffer_cursor, 2)
         buffer_cursor = buffer_cursor + 2
         -- Size
-        local command_size = buffer(buffer_cursor, 4)
+        command_size = buffer(buffer_cursor, 4)
         buffer_cursor = buffer_cursor + 4
         subtree:add_le(vsomeip_client, command_client)
         subtree:add_le(vsomeip_size, command_size)
         pinfo.cols.info:append(" (" ..command_client(1,1)..command_client(0,1).. ")")
     else
         -- Size
-        local command_size = buffer(buffer_cursor, 4)
+        command_size = buffer(buffer_cursor, 4)
         buffer_cursor = buffer_cursor + 4
         subtree:add_le(vsomeip_size, command_size)
     end
@@ -148,13 +183,82 @@ function vsomeip_protocol.dissector(buffer, pinfo, tree)
         local command_name = buffer(buffer_cursor, length - buffer_cursor - 4)
         subtree:add(vsomeip_name, command_name)
 
-        pinfo.cols.info:append(" "..command_name)
+        pinfo.cols.info:append(" "..command_name:string())
+
+    elseif command_name == "VSOMEIP_ASSIGN_CLIENT_ACK" then
+        local command_newclient = buffer(buffer_cursor, 2)
+        buffer_cursor = buffer_cursor + 2
+        subtree:add_le(vsomeip_newclient, command_newclient)
 
     elseif command_name == "VSOMEIP_ROUTING_INFO" then
         local command_entries = buffer(buffer_cursor, length - buffer_cursor - 4)
+        if command_entries:len() > 0 then
+            routing_info_tree = subtree:add("Routing Info Commands")
 
-        subtree:add(vsomeip_entries, command_entries)
+            while command_entries:len() > 0 do
+                local cursor = 0
+                local routing_info_command = command_entries(cursor, 1)
+                cursor = cursor + 1
+                local routing_info_size = command_entries(cursor, 4)
+                cursor = cursor + 4
+                local routing_info_size_client = routing_info_size(0, 4)
+                if routing_info_command(0,1):uint() > 0x01 then
+                    routing_info_size_client = command_entries(cursor, 4)
+                    cursor = cursor + 4
+                end
+                local routing_info_client = command_entries(cursor, 2)
+                cursor = cursor + 2
 
+                routing_info_tree_cmd = routing_info_tree:add(get_routing_info_command_name(routing_info_command:uint()))
+                routing_info_tree_cmd:add_le(vsomeip_routing_info_command, routing_info_command)
+                routing_info_tree_cmd:add_le(vsomeip_routing_info_size, routing_info_size)
+                if routing_info_command(0,1):uint() > 0x01 then
+                    routing_info_tree_cmd:add_le(vsomeip_routing_info_size_client, routing_info_size_client)
+                end
+                routing_info_tree_cmd:add_le(vsomeip_routing_info_client, routing_info_client)
+                if buffer_4_to_int(routing_info_size_client) > 2 then -- if 2 then there will be no address
+                    local routing_info_address = command_entries(cursor, 4)
+                    cursor = cursor + 4
+                    local routing_info_port = command_entries(cursor, 2)
+                    cursor = cursor + 2
+                    routing_info_tree_cmd:add(vsomeip_routing_info_address, routing_info_address)
+                    routing_info_tree_cmd:add_le(vsomeip_routing_info_port, routing_info_port)
+                end
+                if routing_info_command(0,1):uint() > 0x01 then
+                    local routing_info_size_service = command_entries(cursor, 4)
+                    cursor = cursor + 4
+                    routing_info_tree_cmd:add_le(vsomeip_routing_info_size_service, routing_info_size_service)
+
+                    local total_services_info = buffer_4_to_int(routing_info_size_service) / 9
+
+                    if total_services_info > 0 then
+                        routing_info_tree_service = routing_info_tree_cmd:add("Services")
+                        for i = 0, total_services_info - 1,1 do
+                            local routing_info_service_id = command_entries(cursor, 2)
+                            cursor = cursor + 2
+                            local routing_info_instance = command_entries(cursor, 2)
+                            cursor = cursor + 2
+                            local routing_info_major = command_entries(cursor, 1)
+                            cursor = cursor + 1
+                            local routing_info_minor = command_entries(cursor, 4)
+                            cursor = cursor + 4
+
+                            routing_info_tree_service_tree = routing_info_tree_service:add(i .. ': ' .. routing_info_service_id(1,1)..routing_info_service_id(0,1))
+                            routing_info_tree_service_tree:add_le(vsomeip_routing_info_service_id, routing_info_service_id)
+                            routing_info_tree_service_tree:add_le(vsomeip_routing_info_instance, routing_info_instance)
+                            routing_info_tree_service_tree:add_le(vsomeip_routing_info_major, routing_info_major)
+                            routing_info_tree_service_tree:add_le(vsomeip_routing_info_minor, routing_info_minor)
+                        end
+                    end
+
+                end
+
+                if cursor >= command_entries:len() then
+                    break
+                end
+                command_entries = command_entries(cursor, command_entries:len() - cursor)
+            end
+        end
     elseif command_name == "VSOMEIP_OFFER_SERVICE" or command_name == "VSOMEIP_STOP_OFFER_SERVICE" then
         local command_service = buffer(buffer_cursor, 2)
         buffer_cursor = buffer_cursor + 2
@@ -220,6 +324,29 @@ function vsomeip_protocol.dissector(buffer, pinfo, tree)
 
         pinfo.cols.info:append(" [" .. command_service(1,1)..command_service(0,1) ..".".. command_instance(1,1)..command_instance(0,1)
                              .. "." .. command_eventgroup(1,1)..command_eventgroup(0,1) .. "." .. command_event(1,1)..command_event(0,1) .. "]")
+
+    elseif command_name == "VSOMEIP_REQUEST_SERVICE" then
+
+    local total_services_info = buffer_4_to_int(command_size) / 9
+    if total_services_info > 0 then
+        routing_info_tree_service = subtree:add("Services")
+        for i = 0, total_services_info - 1,1 do
+            local routing_info_service_id = buffer(buffer_cursor, 2)
+            buffer_cursor = buffer_cursor + 2
+            local routing_info_instance = buffer(buffer_cursor, 2)
+            buffer_cursor = buffer_cursor + 2
+            local routing_info_major = buffer(buffer_cursor, 1)
+            buffer_cursor = buffer_cursor + 1
+            local routing_info_minor = buffer(buffer_cursor, 4)
+            buffer_cursor = buffer_cursor + 4
+
+            routing_info_tree_service_tree = routing_info_tree_service:add(i .. ': ' .. routing_info_service_id(1,1)..routing_info_service_id(0,1))
+            routing_info_tree_service_tree:add_le(vsomeip_routing_info_service_id, routing_info_service_id)
+            routing_info_tree_service_tree:add_le(vsomeip_routing_info_instance, routing_info_instance)
+            routing_info_tree_service_tree:add_le(vsomeip_routing_info_major, routing_info_major)
+            routing_info_tree_service_tree:add_le(vsomeip_routing_info_minor, routing_info_minor)
+        end
+    end
 
     elseif command_name == "VSOMEIP_RELEASE_SERVICE" then
         local command_service = buffer(buffer_cursor, 2)
@@ -405,6 +532,19 @@ function get_command_name(cmd)
   return cmd_name
 end
 
+function get_routing_info_command_name(cmd)
+    local cmd_name = "RIE_UNKOWN"
+
+    -- ¯\_(ツ)_/¯
+        if cmd == 0x00 then cmd_name = "RIE_ADD_CLIENT"
+    elseif cmd == 0x01 then cmd_name = "RIE_DEL_CLIENT"
+    elseif cmd == 0x02 then cmd_name = "RIE_ADD_SERVICE_INSTANCE"
+    elseif cmd == 0x04 then cmd_name = "RIE_DEL_SERVICE_INSTANCE"
+    end
+
+  return cmd_name
+end
+
 function get_reliability(reliable)
     if reliable == 0 then
         return "UDP"
@@ -423,4 +563,12 @@ function get_offer_type(offer)
     else
         return "UNKOWN"
     end
+end
+
+function buffer_4_to_int(buffer)
+    local length = buffer:len()
+    if length ~= 4 then
+        return 0
+    end
+    return buffer(0,1):uint() + buffer(1,1):uint() * 16  + buffer(2,1):uint() * 256 + buffer(3,1):uint() * 4096
 end
