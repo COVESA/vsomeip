604b2e31b72d90c2da1f7fa2bd295cf5ffada709
andrefesilva <andre.fe.silva@ctw.bmwgroup.com>
2024-10-22 18:08:09 +0100

    Suspend resume tests fix
    Summary
    Suspend resume tests now starts "runner" thread after app->init
    Details
    The goal of the changes in this PR is to move the start of the "runner"
    thread in which the app->start runs from the initialization list of the
    suspend_resume_test_service and only start the thread after the app init.
    The problem that lead to this change was that for some reason, the "runner"
    thread although it should start when the test starts, very often it only started
    after the "start" function of the test that runs the app->init. This is a problem
    since when the "runner" thread starts, it locks a mutex and waits a condition
    variable to be notified to then run the app->start, and the "start" function is
    the one that notifies the condition variable after running the app->init. What
    was happening was that the "runner" thread did not start at the beginning of
    the test, the "start" function would notify the condition variable and only then
    the "runner" thread would start and wait for the condition variable to be
    notified, which never happened since the condition variable was notified
    before the "runner" thread started, leading to a timeout.
    To fix this issue, I removed the initialization of the "runner" thread away
    from the initialization list and only initialized it after the app->init was run,,
    without using condition variables to guarantee that the app->start happens
    after the app->init. Before this fix the issue would occur very frequently
    (>50% of runs) on a VM with roughly the same resources as zuul, and with
    this fix it never failed again locally.

--- test/network_tests/suspend_resume_tests/suspend_resume_test_service.cpp
+++ test/network_tests/suspend_resume_tests/suspend_resume_test_service.cpp
@@ -22,15 +22,11 @@ pid_t daemon_pid__;
 
 class suspend_resume_test_service : public vsomeip_utilities::base_logger {
 public:
-    suspend_resume_test_service()
-        : vsomeip_utilities::base_logger("ATCA", "APPLICATION TEST CLIENT AVAILABILITY"),
-          name_("suspend_resume_test_service"),
-          app_(vsomeip::runtime::get()->create_application(name_)),
-          is_running_(true),
-          is_unblocked_(false),
-          runner_(std::bind(&suspend_resume_test_service::run, this)),
-          sr_runner_(std::bind(&suspend_resume_test_service::sr_run, this)) {
-    }
+    suspend_resume_test_service() :
+        vsomeip_utilities::base_logger("ATCA", "APPLICATION TEST CLIENT AVAILABILITY"),
+        name_("suspend_resume_test_service"),
+        app_(vsomeip::runtime::get()->create_application(name_)), is_running_(true),
+        is_unblocked_(false), sr_runner_(std::bind(&suspend_resume_test_service::sr_run, this)) { }
 
     void run_test() {
 
@@ -55,7 +51,7 @@ private:
     void start() {
 
         app_->init();
-        cv_.notify_one();
+        runner_ = std::thread([this]() { app_->start(); });
     }
 
     void stop() {
@@ -67,16 +63,6 @@ private:
 
         runner_.join();
         sr_runner_.join();
-    }
-
-    void run() {
-
-        {
-            std::unique_lock<std::mutex> its_lock(mutex_);
-            cv_.wait(its_lock);
-        }
-
-        app_->start();
     }
 
     void sr_run() {
@@ -98,30 +84,30 @@ private:
     void register_state_handler() {
 
         app_->register_state_handler(
-            std::bind(&suspend_resume_test_service::on_state, this, std::placeholders::_1));
+                std::bind(&suspend_resume_test_service::on_state, this, std::placeholders::_1));
     }
 
     void register_message_handler() {
 
-        app_->register_message_handler(TEST_SERVICE, TEST_INSTANCE, TEST_METHOD,
-            std::bind(&suspend_resume_test_service::on_message, this,
-                std::placeholders::_1));
+        app_->register_message_handler(
+                TEST_SERVICE, TEST_INSTANCE, TEST_METHOD,
+                std::bind(&suspend_resume_test_service::on_message, this, std::placeholders::_1));
     }
 
     void register_subscription_handler() {
 
-        app_->register_subscription_handler(TEST_SERVICE, TEST_INSTANCE, TEST_EVENTGROUP,
-            std::bind(&suspend_resume_test_service::on_subscribe, this,
-                    std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
+        app_->register_subscription_handler(
+                TEST_SERVICE, TEST_INSTANCE, TEST_EVENTGROUP,
+                std::bind(&suspend_resume_test_service::on_subscribe, this, std::placeholders::_1,
+                          std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
     }
 
     void offer_service() {
-        app_->offer_event(TEST_SERVICE, TEST_INSTANCE, TEST_EVENT, { TEST_EVENTGROUP },
-                vsomeip::event_type_e::ET_FIELD,
-                std::chrono::milliseconds::zero(), false, true, nullptr,
-                vsomeip::reliability_type_e::RT_UNRELIABLE);
+        app_->offer_event(TEST_SERVICE, TEST_INSTANCE, TEST_EVENT, {TEST_EVENTGROUP},
+                          vsomeip::event_type_e::ET_FIELD, std::chrono::milliseconds::zero(), false,
+                          true, nullptr, vsomeip::reliability_type_e::RT_UNRELIABLE);
 
-        vsomeip::byte_t its_data[] = { 0x1, 0x2, 0x3 };
+        vsomeip::byte_t its_data[] = {0x1, 0x2, 0x3};
         auto its_payload = vsomeip::runtime::get()->create_payload();
         its_payload->set_data(its_data, sizeof(its_data));
         app_->notify(TEST_SERVICE, TEST_INSTANCE, TEST_EVENT, its_payload);
@@ -132,27 +118,23 @@ private:
     // handler
     void on_state(vsomeip::state_type_e _state) {
         VSOMEIP_DEBUG << __func__ << "[TEST-srv]: state="
-            << (_state == vsomeip::state_type_e::ST_REGISTERED ?
-                    "registered." : "NOT registered.");
+                      << (_state == vsomeip::state_type_e::ST_REGISTERED ? "registered."
+                                                                         : "NOT registered.");
 
         if (_state == vsomeip::state_type_e::ST_REGISTERED) {
             offer_service();
         }
     }
 
-    void on_message(const std::shared_ptr<vsomeip::message> &_message) {
+    void on_message(const std::shared_ptr<vsomeip::message>& _message) {
 
-        VSOMEIP_DEBUG << __func__ << "[TEST-srv]: Received "
-                << std::hex << std::setw(4) << std::setfill('0')
-                << _message->get_service()
-                << std::hex << std::setw(4) << std::setfill('0')
-                << _message->get_instance()
-                << std::hex << std::setw(4) << std::setfill('0')
-                << _message->get_method();
+        VSOMEIP_DEBUG << __func__ << "[TEST-srv]: Received " << std::hex << std::setw(4)
+                      << std::setfill('0') << _message->get_service() << std::hex << std::setw(4)
+                      << std::setfill('0') << _message->get_instance() << std::hex << std::setw(4)
+                      << std::setfill('0') << _message->get_method();
 
-        if (_message->get_service() == TEST_SERVICE
-                && _message->get_instance() == TEST_INSTANCE
-                && _message->get_method() == TEST_METHOD) {
+        if (_message->get_service() == TEST_SERVICE && _message->get_instance() == TEST_INSTANCE
+            && _message->get_method() == TEST_METHOD) {
 
             if (_message->get_payload()->get_length() == 1) {
 
@@ -165,16 +147,14 @@ private:
                 case TEST_STOP:
                     cv_.notify_one();
                     break;
-                default:
-                    ;
+                default:;
                 }
             }
         }
     }
 
-    bool on_subscribe(vsomeip::client_t _client,
-            vsomeip::uid_t _uid, vsomeip::gid_t _gid,
-            bool _is_subscribe) {
+    bool on_subscribe(vsomeip::client_t _client, vsomeip::uid_t _uid, vsomeip::gid_t _gid,
+                      bool _is_subscribe) {
 
         (void)_client;
         (void)_uid;
@@ -199,8 +179,7 @@ private: // members
     std::thread sr_runner_;
 };
 
-TEST(suspend_resume_test, fast)
-{
+TEST(suspend_resume_test, fast) {
     suspend_resume_test_service its_service;
     its_service.run_test();
 }
