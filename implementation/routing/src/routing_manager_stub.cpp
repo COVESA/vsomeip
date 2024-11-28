28223e21eea60c5cd92dfb6dc6b11fe727d6711f
falk-haleytek <daniel.falk@haleytek.com>
2024-11-28 17:05:22 +0100

    Log offers on first occurrence in routing manager (#728)
    For debugging purposes, the routing manager has been updated to log
    the first time a new service is detected to be offered.

--- implementation/routing/src/routing_manager_stub.cpp
+++ implementation/routing/src/routing_manager_stub.cpp
@@ -1128,6 +1128,13 @@ routing_manager_stub::on_net_state_change(
 
 void routing_manager_stub::on_offer_service(client_t _client,
         service_t _service, instance_t _instance, major_version_t _major, minor_version_t _minor) {
+
+    VSOMEIP_DEBUG << "routing_manager_stub::" << __func__ << ": ON_OFFER_SERVICE("
+        << std::hex << std::setw(4) << std::setfill('0') << _client <<"): ["
+        << std::hex << std::setw(4) << std::setfill('0') << _service << "."
+        << std::hex << std::setw(4) << std::setfill('0') << _instance
+        << ":" << std::dec << int(_major) << "." << std::dec << _minor << "]";
+
     if (_client == host_->get_client()) {
         create_local_receiver();
     }
