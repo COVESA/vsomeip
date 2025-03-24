cdb1160b908ee35763ae00c530e90ff9cd88cbb3
Duarte Fonseca <42354362+duartenfonseca@users.noreply.github.com>
2025-03-24 17:03:35 +0000

    vSomeIP-Lib 3.5.5 (#871)
    Changes:
    
    - Fix build issue on Ubuntu 24.04
    - Fixing change that was introduced on Fix for Find messages commit
    - Changing mandatory configuration files parsing method
    - Testing possible missing break
    - reset force_initial_events
    - Reintroduce VSOMEIP_CONFIG
    - Enable partial valgrind subprocess tracking
    - Ensure endpoint survives send operation
    - Avoid hanging request service commands
    - Fix initial event tests
    - Ensure state value before send_pending_commands
    - Clean-up service info on registration
    - Add and expose settings for SD find debounce as config options
    - Simplify "Add and expose settings for SD find debounce as config options"
    - Consistently use uid_t/gid_t for credentials
    - Skip valgrind initialevents network test
    - Fix NSM logs with empty Container Id strings
    - Fix issue on STR re registration for local tcp
    - Fix data race due to multiple multicast SD message timeouts
    - Remove leftover routing_info_entry_e
    - Remove patches applied as fix
    - Fix reboot on vnet toggle
    - check client queue_size first
    - Add helgrind dubious locks suppresses
    - Reintroduced server endpoints restart on resume
    - Ensure service offers after route toggling
    - Add a keepalive mechanism to the routing_manager_client
    - fix failures on external_local_routing_test
    - Make local client keep alive mandatory
    - Use variable linger for local TCP
    - fixing vsomeip install without vsomeip_ctrl
    - Fix port format
    - Add vsomeip_device.json to mandatory files
    - Revert Fix NSM logs with empty Container Id strings
    
    Co-authored-by: Duarte Fonseca <Duarte.Fonseca@ctw.bmwgroup.com>

--- Android.mk
+++ Android.mk
@@ -100,7 +100,7 @@ LOCAL_CFLAGS :=  \
     -frtti \
     -fexceptions \
     -DWITHOUT_SYSTEMD \
-    -DVSOMEIP_VERSION=\"3.5.4\" \
+    -DVSOMEIP_VERSION=\"3.5.5\" \
     -DVSOMEIP_BASE_PATH=\"/vendor/run/someip/\" \
     -Wno-unused-parameter \
     -Wno-non-virtual-dtor \
@@ -147,7 +147,7 @@ LOCAL_CFLAGS := \
     -frtti \
     -fexceptions \
     -DWITHOUT_SYSTEMD \
-    -DVSOMEIP_VERSION=\"3.5.4\" \
+    -DVSOMEIP_VERSION=\"3.5.5\" \
     -DVSOMEIP_BASE_PATH=\"/vendor/run/someip/\" \
     -Wno-unused-parameter \
     -Wno-non-virtual-dtor \
@@ -194,8 +194,8 @@ LOCAL_CFLAGS :=  \
     -frtti \
     -fexceptions \
     -DWITHOUT_SYSTEMD \
-    -DVSOMEIP_VERSION=\"3.5.4\" \
-    -DVSOMEIP_COMPAT_VERSION=\"3.5.4\" \
+    -DVSOMEIP_VERSION=\"3.5.5\" \
+    -DVSOMEIP_COMPAT_VERSION=\"3.5.5\" \
     -DVSOMEIP_BASE_PATH=\"/vendor/run/someip/\" \
     -Wno-unused-parameter \
     -Wno-non-virtual-dtor \
