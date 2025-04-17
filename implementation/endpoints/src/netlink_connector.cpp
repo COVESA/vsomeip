ed788f69b41b01434afa99758cf17ff98a694875
fabiofcmonteiro <fabio.fc.monteiro@ctw.bmwgroup.com>
2025-04-17 12:12:32 +0100

    Remove interface index in RTM_DELLINK
    Summary
    Removes the interface index when we get a RTM_DELLINK
    Details
    Resetting the flag on DELADDR/DELLINK results in incorrect/no reporting when the
    interfaces comes up again. Erase the entry instead. It will be redone on NEWADDR
    when the interface/address comes up again.

--- implementation/endpoints/src/netlink_connector.cpp
+++ implementation/endpoints/src/netlink_connector.cpp
@@ -182,8 +182,8 @@ void netlink_connector::receive_cbk(boost::system::error_code const &_error,
                 case RTM_DELLINK:
                     {
                         struct ifinfomsg *ifi = (ifinfomsg *)NLMSG_DATA(nlh);
-                        net_if_flags_[ifi->ifi_index] = ifi->ifi_flags;
                         if (net_if_index_for_address_ == ifi->ifi_index) {
+                            net_if_flags_.erase(ifi->ifi_index);
                             if_indextoname(static_cast<unsigned int>(ifi->ifi_index),ifname);
                             net_if_index_for_address_ = 0;
                             handler_(true, ifname, false);
