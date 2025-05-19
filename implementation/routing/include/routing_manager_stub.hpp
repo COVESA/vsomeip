27f44733da79767ba56765a4e6c0788c59afa45a
danielsantiago <daniel.santiago@ctw.bmwgroup.com>
2025-05-19 18:32:01 +0100

    Fix NSM logs with empty Container Id strings
    Summary
    Implemented config_command in handle_requests
    Details
    Implemented the config_command in handle_requests
    to fix NSM logs with an empty "Container ID" string.
    The container ID was empty because
    the config_command was not received by
    the application (service), only between the
    clients and the daemon, which did not provide
    the container IDs to the application.
    To resolve the issue and avoid the problems
    caused by dd25ea14051e383f31c7f8a2418edb476209faeb,
    the config_command was implemented to be
    sent from the daemon to the application (service),
    ensuring that the config_command message is only sent
    after the routing_info is sent,
    thereby guaranteeing that the routing_info is processed first.

--- implementation/routing/include/routing_manager_stub.hpp
+++ implementation/routing/include/routing_manager_stub.hpp
@@ -133,6 +133,8 @@ public:
 
     void add_known_client(client_t _client, const std::string &_client_host);
 
+    std::string get_env(client_t _client) const;
+
     void send_suspend() const;
 
     void remove_subscriptions(port_t _local_port,
@@ -203,6 +205,8 @@ private:
             protocol::routing_info_entry &_entry);
     void send_client_routing_info(const client_t _target,
             std::vector<protocol::routing_info_entry> &&_entries);
+    void send_client_config_command(const client_t _client, const client_t _target);
+
     void send_client_credentials(client_t _target, std::set<std::pair<uid_t, gid_t>> &_credentials);
 
     void on_client_id_timer_expired(boost::system::error_code const &_error);
