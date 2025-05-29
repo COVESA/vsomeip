9c28c6581298e58376295ba7b8aedce5a5b39ae2
Matthew Russell <matthew.g.russell@gmail.com>
2025-05-29 13:54:56 -0400

    fixup! fixup! fixup! fixup! Code Quality: Address shadowing
    Switched to scoped_lock

--- implementation/endpoints/src/tcp_client_endpoint_impl.cpp
+++ implementation/endpoints/src/tcp_client_endpoint_impl.cpp
@@ -89,7 +89,7 @@ void tcp_client_endpoint_impl::restart(bool _force) {
         }
         std::string address_port_local;
         {
-            std::lock_guard<std::mutex> its_lock(self->socket_mutex_);
+            std::scoped_lock<std::mutex> its_lock(self->socket_mutex_);
             address_port_local = self->get_address_port_local();
             self->shutdown_and_close_socket_unlocked(true);
             self->recv_buffer_ = std::make_shared<message_buffer_t>(self->recv_buffer_size_initial_, 0);
@@ -98,7 +98,7 @@ void tcp_client_endpoint_impl::restart(bool _force) {
         self->was_not_connected_ = true;
         self->reconnect_counter_ = 0;
         {
-            std::lock_guard<std::recursive_mutex> its_lock(self->mutex_);
+            std::scoped_lock<std::recursive_mutex> its_lock(self->mutex_);
             for (const auto &q : self->queue_) {
                 const service_t its_service = bithelper::read_uint16_be(&(*q.first)[VSOMEIP_SERVICE_POS_MIN]);
                 const method_t its_method   = bithelper::read_uint16_be(&(*q.first)[VSOMEIP_METHOD_POS_MIN]);
@@ -208,7 +208,7 @@ void tcp_client_endpoint_impl::connect() {
                 }
                 std::size_t operations_cancelled;
                 {
-                    std::lock_guard<std::mutex> its_lock_inner(connecting_timer_mutex_);
+                    std::scoped_lock<std::mutex> its_lock_inner(connecting_timer_mutex_);
                     operations_cancelled = connecting_timer_.cancel();
                 }
                 if (operations_cancelled != 0) {
@@ -251,7 +251,7 @@ void tcp_client_endpoint_impl::connect() {
                         << "(" << its_error.value() << "): " << its_error.message();
         std::size_t operations_cancelled;
         {
-            std::lock_guard<std::mutex> its_lock_inner(connecting_timer_mutex_);
+            std::scoped_lock<std::mutex> its_lock_inner(connecting_timer_mutex_);
             operations_cancelled = connecting_timer_.cancel();
         }
         if (operations_cancelled != 0) {
@@ -267,7 +267,7 @@ void tcp_client_endpoint_impl::connect() {
 void tcp_client_endpoint_impl::receive() {
     message_buffer_ptr_t its_recv_buffer;
     {
-        std::lock_guard<std::mutex> its_lock(socket_mutex_);
+        std::scoped_lock<std::mutex> its_lock(socket_mutex_);
         its_recv_buffer = recv_buffer_;
     }
     auto self = std::dynamic_pointer_cast< tcp_client_endpoint_impl >(shared_from_this());
@@ -279,7 +279,7 @@ void tcp_client_endpoint_impl::receive() {
 void tcp_client_endpoint_impl::receive(message_buffer_ptr_t  _recv_buffer,
              std::size_t _recv_buffer_size,
              std::size_t _missing_capacity) {
-    std::lock_guard<std::mutex> its_lock(socket_mutex_);
+    std::scoped_lock<std::mutex> its_lock(socket_mutex_);
     if(socket_->is_open()) {
         const std::size_t its_capacity(_recv_buffer->capacity());
         size_t buffer_size = its_capacity - _recv_buffer_size;
@@ -356,7 +356,7 @@ void tcp_client_endpoint_impl::send_queued(std::pair<message_buffer_ptr_t, uint3
     VSOMEIP_INFO << msg.str();
 #endif
     {
-        std::lock_guard<std::mutex> its_lock(socket_mutex_);
+        std::scoped_lock<std::mutex> its_lock(socket_mutex_);
         if (socket_->is_open()) {
             boost::asio::async_write(
                 *socket_,
@@ -409,7 +409,7 @@ uint16_t tcp_client_endpoint_impl::get_local_port() const {
     uint16_t its_port(0);
 
     // Local port may be zero, if no client ports are configured
-    std::lock_guard<std::mutex> its_lock(socket_mutex_);
+    std::scoped_lock<std::mutex> its_lock(socket_mutex_);
     if (socket_->is_open()) {
         boost::system::error_code its_error;
         endpoint_type its_local = socket_->local_endpoint(its_error);
@@ -426,7 +426,7 @@ uint16_t tcp_client_endpoint_impl::get_local_port() const {
 }
 
 void tcp_client_endpoint_impl::set_local_port() {
-    std::lock_guard<std::mutex> its_lock(socket_mutex_);
+    std::scoped_lock<std::mutex> its_lock(socket_mutex_);
     boost::system::error_code its_error;
     if (socket_->is_open()) {
         endpoint_type its_endpoint = socket_->local_endpoint(its_error);
@@ -444,7 +444,7 @@ void tcp_client_endpoint_impl::set_local_port() {
 
 void tcp_client_endpoint_impl::set_local_port(port_t _port) {
 
-    std::lock_guard<std::mutex> its_lock(socket_mutex_);
+    std::scoped_lock<std::mutex> its_lock(socket_mutex_);
     if (!socket_->is_open()) {
         local_.port(_port);
     } else {
@@ -879,11 +879,11 @@ void tcp_client_endpoint_impl::handle_recv_buffer_exception(
     VSOMEIP_ERROR << its_message.str();
     _recv_buffer->clear();
     {
-        std::lock_guard<std::recursive_mutex> its_lock(mutex_);
+        std::scoped_lock<std::recursive_mutex> its_lock(mutex_);
         sending_blocked_ = true;
     }
     {
-        std::lock_guard<std::mutex> its_lock(connect_timer_mutex_);
+        std::scoped_lock<std::mutex> its_lock(connect_timer_mutex_);
         boost::system::error_code ec;
         connect_timer_.cancel(ec);
     }
@@ -907,13 +907,13 @@ void tcp_client_endpoint_impl::print_status() {
     std::size_t its_queue_size(0);
     std::size_t its_receive_buffer_capacity(0);
     {
-        std::lock_guard<std::recursive_mutex> its_lock(mutex_);
+        std::scoped_lock<std::recursive_mutex> its_lock(mutex_);
         its_queue_size = queue_.size();
         its_data_size = queue_size_;
     }
     std::string local;
     {
-        std::lock_guard<std::mutex> its_lock(socket_mutex_);
+        std::scoped_lock<std::mutex> its_lock(socket_mutex_);
         local = get_address_port_local();
         its_receive_buffer_capacity = recv_buffer_->capacity();
     }
@@ -936,7 +936,7 @@ void tcp_client_endpoint_impl::send_cbk(boost::system::error_code const &_error,
                                         const message_buffer_ptr_t& _sent_msg) {
     (void)_bytes;
 
-    std::lock_guard<std::recursive_mutex> its_lock(mutex_);
+    std::scoped_lock<std::recursive_mutex> its_lock(mutex_);
     boost::system::error_code ec;
     sent_timer_.cancel(ec);
 
