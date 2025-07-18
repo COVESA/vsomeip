6912fff10f2067b73417067c6ccbeb0bd73fb819
donatellobussacchini <Donatello.Bussacchini@bmw.de>
2025-07-18 17:08:39 +0200

    NetLink changes
    Summary
    As suggested, this PR groups together all changes made to NetLink.
    Details
    The following changes have been made:
    
     -add unit tests
     -optimize get link request by filtering the output
     -fix IPv6 handling
     -optimize bind parameters to the data really needed
     -improve error handling
     -reduce buffer size
     -remove dead code and add trace

--- implementation/endpoints/include/netlink_connector.hpp
+++ implementation/endpoints/include/netlink_connector.hpp
@@ -137,20 +137,27 @@ private:
     int proto;
 };
 
+typedef std::function<void(bool, // true = is interface, false = is route
+                           std::string, // interface name
+                           bool) // available?
+                      >
+        net_if_changed_handler_t;
+
 class netlink_connector : public abstract_netlink_connector,
                           public std::enable_shared_from_this<netlink_connector> {
+    enum class state_e { INIT, RESET, DOWN, UP, UP_MULTICAST };
+
 public:
-    netlink_connector(boost::asio::io_context &_io, const boost::asio::ip::address &_address,
-                        const boost::asio::ip::address &_multicast_address,
-                        bool _is_requiring_link = true):
-        net_if_index_for_address_(0),
-        handler_(nullptr),
-        socket_(_io),
-        recv_buffer_(recv_buffer_size, 0),
-        address_(_address),
-        multicast_address_(_multicast_address),
-        is_requiring_link_(_is_requiring_link) {
-    }
+    netlink_connector(boost::asio::io_context& _io, const boost::asio::ip::address& _address,
+                      const boost::asio::ip::address& _multicast_address,
+                      bool _is_requiring_link = true) :
+        net_if_index_for_address_(0), handler_(nullptr), socket_(_io),
+        recv_buffer_(_multicast_address.is_v4() || _multicast_address.is_v6()
+                             ? multicast_buffer_size
+                             : default_buffer_size,
+                     0),
+        address_(_address), multicast_address_(_multicast_address),
+        is_requiring_link_(_is_requiring_link) { }
     ~netlink_connector() {}
 
     void register_net_if_changes_handler(const net_if_changed_handler_t& _handler) override;
@@ -160,19 +167,17 @@ public:
     void stop() override;
 
 private:
-    bool has_address(struct ifaddrmsg * ifa_struct,
-            size_t length,
-            const unsigned int address);
+    void set_state(state_e _state);
+
+    bool has_address(const struct ifaddrmsg* ifa_struct, size_t length) const;
     void send_ifa_request(std::uint32_t _retry = 0);
     void send_ifi_request(std::uint32_t _retry = 0);
     void send_rt_request(std::uint32_t _retry = 0);
-    void handle_netlink_error(struct nlmsgerr *_error_msg);
 
     void receive_cbk(boost::system::error_code const &_error, std::size_t _bytes);
     void send_cbk(boost::system::error_code const &_error, std::size_t _bytes);
 
-    bool check_sd_multicast_route_match(struct rtmsg* _routemsg,
-                                        size_t _length,
+    bool check_sd_multicast_route_match(const struct rtmsg* _routemsg, size_t _length,
                                         std::string* _routename) const;
 
     std::map<int, unsigned int> net_if_flags_;
@@ -183,12 +188,34 @@ private:
     std::mutex socket_mutex_;
     boost::asio::basic_raw_socket<nl_protocol> socket_;
 
-    const size_t recv_buffer_size = 16384;
+    // The following observations were made: setting SO_RCVBUF to 8192 or
+    // 16384 causes an ENOBUFS when NetLink is configured to listen for
+    // IPv6 messages. However, the largest message was only 940 bytes and
+    // the total size of all messages wass less than 1500 bytes.
+    // Below is an attempt to explain this behaviour. First, Linux treats
+    // the SO_RCVBUF parameter as a maximum value, which is 256K by default.
+    // It simulates the POSIX behaviour with the SOCK_RCVBUF_LOCK flag.
+    // https://elixir.bootlin.com/linux/v6.15.1/source/net/core/sock.c#L982
+    // In addition, NetLink manages this parameter in its own way.
+    // https://elixir.bootlin.com/linux/v6.15.1/C/ident/sk_rcvbuf
+    // It seems that Linux allocates a block (sk_buff) for each message and,
+    // as a result, SO_RCVBUFF limits not only the size of messages but also
+    // their number.
+    // As consequence, we do not modify the SO_RCVBUF parameter. The
+    // consequences are unpredictable, and it does not reduce the memory
+    // used. But we limit the size of the application buffer.
+
+    const size_t multicast_buffer_size = 16384;
+    const size_t default_buffer_size = 8192;
     message_buffer_t recv_buffer_;
 
     boost::asio::ip::address address_;
     boost::asio::ip::address multicast_address_;
     bool is_requiring_link_;
+    bool interface_sync_done_ = false;
+    bool route_sync_done_ = false;
+    bool multicast_route_found_ = false;
+    state_e current_state_ = state_e::INIT;
 
     static const std::uint32_t max_retries_ = VSOMEIP_MAX_NETLINK_RETRIES;
     static const std::uint32_t retry_bit_shift_ = 8;
