98d1ea4a4fabcc862dbd87e1202d7d4143250f1d
brunoldsilva <bruno.ld.silva@ctw.bmwgroup.com>
2025-08-13 13:07:33 +0100

    Check availability on send
    Check if a service is available before sending a request to it.
    
    This would prevent us from trying to process a request which might not
    reach its destination, and allows us to immediately report an error to
    the application.
    
    Process offer service before on_availability callback being invoked,
    with previous order, clients could send a request while the service is
    still not added to the control map.
    
    Npdu test is updated to enforce the clients to request services before
    sending any request type message, without the request, the host will not
    send the ADD_SERVICE routing info command and consequently on the client
    side the service will constantly be assessed as unavailable.

--- implementation/routing/include/routing_manager_stub.hpp
+++ implementation/routing/include/routing_manager_stub.hpp
@@ -112,6 +112,13 @@ public:
 
     routing_state_e get_routing_state();
 
+    /**
+     * @brief Checks on the routing_info map if a service is being offered by an external peer.
+     *
+     * @return True if offered, false otherwise.
+     */
+    bool is_remotely_available(service_t _service, instance_t _instance, major_version_t _major) const;
+
 private:
     void broadcast(const std::vector<byte_t>& _command) const;
 
