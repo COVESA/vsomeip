075db709e033ffdf1194761a298b270c040e435f
Rui Graça <rui.graca@ctw.bmwgroup.com>
2025-09-25 15:12:35 +0100

    add_guest only updates rmc
    Follow up of
    The add_guest updating the local_endpoints_ interferes with the (de)registration process.
    This change reverts the add_guest for the old functionality, only for the routing host.
    This topic needs to be revisited.
    During STR, if a client sends a deregistration, and the host only processes it on resume,
    the client will always fail the first re registration attempt.

--- implementation/routing/src/routing_manager_impl.cpp
+++ implementation/routing/src/routing_manager_impl.cpp
@@ -4301,7 +4301,8 @@ bool routing_manager_impl::get_guest(client_t _client, boost::asio::ip::address&
 
 void routing_manager_impl::add_guest(client_t _client, const boost::asio::ip::address& _address, port_t _port) {
 
-    routing_manager_base::add_guest(_client, _address, _port);
+    std::scoped_lock its_lock(guests_mutex_);
+    guests_[_client] = std::make_pair(_address, _port);
 }
 
 void routing_manager_impl::remove_guest(client_t _client) {
