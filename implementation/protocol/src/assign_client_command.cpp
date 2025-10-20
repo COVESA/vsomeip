0ea27ec83d5e79baedc210260896d569a5957d0b
joelpinto <joel.pinto@ctw.bmwgroup.com>
2025-10-20 12:58:54 +0100

    Deserialize full name on assign_client command
    vSomeIP assign client command comprehends the following fields:
    
    Desired client id (if != 0xFFFF);
    Application name size;
    Application name;
    
    If an application is called test, the sent size will be 4 (null character not included).
    Currently, when parsing the assign_client command, the name is stripped of its last
    character, this might lead to a possible race condition if two applications name differ
    by the last character e.g., client1 and client2.

--- implementation/protocol/src/assign_client_command.cpp
+++ implementation/protocol/src/assign_client_command.cpp
@@ -53,7 +53,7 @@ void assign_client_command::deserialize(const std::vector<byte_t>& _buffer, erro
 
     // name?
     if (size_ > 0)
-        name_.assign(&_buffer[COMMAND_POSITION_PAYLOAD], &_buffer[_buffer.size() - 1]);
+        name_.assign(&_buffer[COMMAND_POSITION_PAYLOAD], &_buffer[_buffer.size()]);
 }
 
 std::string assign_client_command::get_name() const {
