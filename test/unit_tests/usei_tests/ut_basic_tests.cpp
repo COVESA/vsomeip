8bb2690e1424d71f669616f9884527576da5635e
Victor Carvalho <victor.carvalho@ctw.bmwgroup.com>
2025-10-23 11:45:28 +0100

    usei, unit tests
    Unit tests of usei to reproduce time-related issues.
    I tried to adapt a POC usei network test, but the network tests are blackbox tests,
    and we cannot create routing_manager_stub or
    endpoint_manager_impl. Changing the interface was considered,
    but it would not pass review. Another solution was to use debug
    symbols to find the private methods, but that would be too
    much. For these reasons, unit tests are developed.

---
+++ test/unit_tests/usei_tests/ut_basic_tests.cpp
@@ -0,0 +1,159 @@
+// Copyright (C) 2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <array>
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <common/utility.hpp>
+
+#include "mocked_vsomeip_dependencies.hpp"
+
+struct usei_fixture : public ::testing::Test {
+    std::shared_ptr<boost::asio::io_context> context_;
+    std::shared_ptr<boost::asio::io_context::work> work_guard_;
+    std::thread mainloop_;
+    std::shared_ptr<vsomeip_v3::udp_server_endpoint_impl> server_;
+    std::shared_ptr<mock_endpoint_host> endpoint_;
+    std::shared_ptr<mock_routing_host> routing_;
+    boost::asio::ip::udp::endpoint unicast_parameters_{boost::asio::ip::make_address("127.0.0.1"), 5000};
+    boost::asio::ip::udp::endpoint tester_parameters_{boost::asio::ip::make_address("127.0.0.1"), 5001};
+    int udp_socket_{-1};
+
+    void SetUp() override {
+        // Boost ASIO main loop
+        context_ = std::make_shared<boost::asio::io_context>();
+        work_guard_ = std::make_shared<boost::asio::io_context::work>(*context_);
+        mainloop_ = std::thread([this] { context_->run(); });
+
+        // VSOMEIP objects
+        endpoint_ = std::make_shared<mock_endpoint_host>();
+        routing_ = std::make_shared<mock_routing_host>();
+        server_ = std::make_shared<vsomeip_v3::udp_server_endpoint_impl>(
+                endpoint_, routing_, *context_, std::make_shared<vsomeip_v3::cfg::configuration_impl>("usei_fixture.json"));
+
+        // UDP tester socket
+        udp_socket_ = ::socket(AF_INET, SOCK_DGRAM, 0);
+        ASSERT_GE(udp_socket_, 0);
+
+        struct sockaddr_in tester_address { };
+        tester_address.sin_family = AF_INET;
+        tester_address.sin_addr.s_addr = htonl(tester_parameters_.address().to_v4().to_uint());
+        tester_address.sin_port = htons(tester_parameters_.port());
+
+        int result = ::bind(udp_socket_, reinterpret_cast<const struct sockaddr*>(&tester_address), sizeof(tester_address));
+        ASSERT_EQ(result, 0);
+    }
+
+    void TearDown() override {
+        // UDP tester socket
+        ::close(udp_socket_);
+
+        // VSOMEIP objects
+        server_.reset();
+        routing_.reset();
+        endpoint_.reset();
+
+        // Boost ASIO main loop
+        context_->stop();
+        mainloop_.join();
+        context_.reset();
+        work_guard_.reset();
+    }
+
+    void send(const boost::asio::ip::udp::endpoint& target, const std::byte* buffer, std::size_t len) {
+        struct sockaddr_in target_address { };
+
+        target_address.sin_family = AF_INET;
+        target_address.sin_addr.s_addr = htonl(target.address().to_v4().to_uint());
+        target_address.sin_port = htons(target.port());
+
+        ssize_t result =
+                ::sendto(udp_socket_, buffer, len, 0, reinterpret_cast<const struct sockaddr*>(&target_address), sizeof(target_address));
+        ASSERT_EQ(result, len);
+    }
+
+    template<size_t N>
+    void send(const boost::asio::ip::udp::endpoint& target, const std::array<std::byte, N>& data) {
+        send(target, data.data(), N);
+    }
+};
+
+// https://stackoverflow.com/questions/45172052/correct-way-to-initialize-a-container-of-stdbyte
+template<typename... Ts>
+std::array<std::byte, sizeof...(Ts)> make_bytes(Ts&&... args) noexcept {
+    return {std::byte(std::forward<Ts>(args))...};
+}
+
+using namespace testing;
+
+TEST_F(usei_fixture, basic) {
+    using namespace std::chrono_literals;
+
+    std::mutex sync;
+    bool received{false};
+    std::condition_variable event;
+
+    EXPECT_CALL(*routing_, on_message)
+            .WillOnce([&](const vsomeip_v3::byte_t*, vsomeip_v3::length_t, vsomeip_v3::endpoint*, bool, vsomeip_v3::client_t,
+                          const vsomeip_sec_client_t*, const boost::asio::ip::address&, uint16_t) {
+                std::unique_lock lock(sync);
+                received = true;
+                event.notify_one();
+            });
+
+    boost::system::error_code error;
+    server_->init(unicast_parameters_, error);
+    server_->start();
+
+    send(unicast_parameters_, make_bytes(0x01, 0x02, 0x03, 0x04, 0x00, 0x00, 0x00, 0x08, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C));
+
+    std::unique_lock lock(sync);
+    EXPECT_EQ(event.wait_for(lock, 5s, [&] { return received; }), true);
+
+    server_->stop();
+}
+
+TEST_F(usei_fixture, no_overwrite_during_restart) {
+    constexpr size_t MESSAGE_SENT_COUNT = 10000;
+    using namespace std::chrono_literals;
+    std::atomic<bool> finished{false};
+
+    EXPECT_CALL(*endpoint_, on_error).Times(0);
+    EXPECT_CALL(*routing_, on_message).Times(AtLeast(MESSAGE_SENT_COUNT / 3));
+
+    boost::system::error_code error;
+    server_->init(unicast_parameters_, error);
+    server_->start();
+
+    auto producer = std::thread([&] {
+        for (size_t i = 0; i < MESSAGE_SENT_COUNT; ++i) {
+            send(unicast_parameters_,
+                 make_bytes(0x01, 0x02, 0x03, 0x04, 0x00, 0x00, 0x00, 0x08, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C));
+            send(unicast_parameters_,
+                 make_bytes(0x01, 0x02, 0x03, 0x04, 0x00, 0x00, 0x00, 0x09, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D));
+            std::this_thread::sleep_for(10us);
+        }
+        finished.store(true);
+    });
+
+    while (!finished.load()) {
+        server_->restart(true);
+        std::this_thread::sleep_for(1ms);
+    }
+
+    producer.join();
+    server_->stop();
+}
