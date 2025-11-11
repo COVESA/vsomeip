c121dbb6f471f9b1d51db13ed2fa0e75f7c56abd
duartefonseca <duarte.fonseca@ctw.bmwgroup.com>
2025-11-11 12:46:44 +0000

    vSomeIP-Lib 3.6.1 Release
    Deliver service availability only after endpoint creation
    set tcp quick ack
    Fix remote subscription expiration
    new network-test for start-stop-start application
    endpoint: drop unused state
    Update subscribe_notify_test_one_tests.md
    zuul: catch all valgrind failures
    build: enable CMAKE_EXPORT_COMPILE_COMMANDS
    endpoint: no waiting when stopping due to error
    app: fail early, fail fast for unknown messages
    tests: add another fake_socket test
    Introduce a thread-safe timer utility
    rmi: create local receiver on start
    fix race condition in connections_ map changes
    Fix payload tests return codes
    Fix/doc offer_test
    Support compilation with gcc15
    tests: updates shebang line to interpret as bash
    Fix network_tests/routing_tests
    Revert "IO threads exit mitigation "
    Fix and document second_address tests
    tests: add regression test

--- Android.mk
+++ Android.mk
@@ -101,7 +101,7 @@ LOCAL_CFLAGS :=  \
     -frtti \
     -fexceptions \
     -DWITHOUT_SYSTEMD \
-    -DVSOMEIP_VERSION=\"3.6.0\" \
+    -DVSOMEIP_VERSION=\"3.6.1\" \
     -DVSOMEIP_BASE_PATH=\"/vendor/run/someip/\" \
     -Wno-unused-parameter \
     -Wno-non-virtual-dtor \
@@ -160,7 +160,7 @@ LOCAL_CFLAGS := \
     -frtti \
     -fexceptions \
     -DWITHOUT_SYSTEMD \
-    -DVSOMEIP_VERSION=\"3.6.0\" \
+    -DVSOMEIP_VERSION=\"3.6.1\" \
     -DVSOMEIP_BASE_PATH=\"/vendor/run/someip/\" \
     -Wno-unused-parameter \
     -Wno-non-virtual-dtor \
