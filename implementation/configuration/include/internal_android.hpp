73ba5635c6678080647e283bbe4db2e8936a4f82
ricardolleite <ricardo.l.leite@bmw.de>
2025-11-20 16:39:22 +0100

    le: fix max-connection-attempts
    Silly regression from 7b3dedc, was causing local endpoints to
    re-connect forever
    When using TCP, it can cause "Cannot assign requested address", due to
    old-endpoints-connecting-forever mixed with new endpoint using the same
    address

--- implementation/configuration/include/internal_android.hpp
+++ implementation/configuration/include/internal_android.hpp
@@ -156,8 +156,7 @@ inline constexpr std::uint32_t QUEUE_SIZE_UNLIMITED = std::numeric_limits<std::u
 #define VSOMEIP_DEFAULT_NPDU_MAXIMUM_RETENTION_NANO  5 * 1000 * 1000
 
 inline constexpr std::uint32_t MAX_RECONNECTS_UNLIMITED = std::numeric_limits<std::uint32_t>::max();
-inline constexpr std::uint32_t MAX_RECONNECTS_LOCAL_UDS = 13;
-inline constexpr std::uint32_t MAX_RECONNECTS_LOCAL_TCP = 5;
+inline constexpr std::uint32_t MAX_RECONNECTS_LOCAL = 5;
 
 inline constexpr uid_t ANY_UID = (std::numeric_limits<uid_t>::max)();
 inline constexpr gid_t ANY_GID = (std::numeric_limits<gid_t>::max)();
