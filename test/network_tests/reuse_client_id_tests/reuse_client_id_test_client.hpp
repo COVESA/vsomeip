e23c061d97b5cf8599d2e7468b1e4e00b9e02d20
goncaloamaral <goncalo.amaral@ctw.bmwgroup.com>
2025-12-10 20:12:52 +0000

    add reuse_client_id_tests
    add reuse_client_id_tests
    Add some tests to check application behavior when 2 apps have
    the same name and clientID.

---
+++ test/network_tests/reuse_client_id_tests/reuse_client_id_test_client.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef REUSE_CLIENT_ID_TEST_CLIENT_HPP_
+#define REUSE_CLIENT_ID_TEST_CLIENT_HPP_
+
+#include <mutex>
+#include <thread>
+
+#include <common/vsomeip_app_utilities.hpp>
+#include <gtest/gtest.h>
+#include <vsomeip/vsomeip.hpp>
+#include "reuse_client_id_test_globals.hpp"
+
+class reuse_client_id_test_client {
+public:
+    reuse_client_id_test_client(const char* app_name_, uint32_t _app_id);
+
+    void on_state(vsomeip::state_type_e _state);
+    void run();
+
+private:
+    std::shared_ptr<vsomeip::application> app_;
+    uint32_t app_id_;
+    std::atomic<bool> is_registered;
+    bool restarted{false};
+    reuse_client_id::reuse_client_id_test_interprocess_sync* ip_sync{nullptr};
+    std::atomic<bool> stopping{false};
+};
+
+#endif // REUSE_CLIENT_ID_TEST_CLIENT_HPP_
