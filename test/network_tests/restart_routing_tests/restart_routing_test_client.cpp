c7f9ea08536937fe9433bdc3b9cf8a369515f70d
danielsantiago <daniel.santiago@ctw.bmwgroup.com>
2026-01-23 15:13:17 +0000

    Remove magic sleeps for IPC
    Remove magic sleeps for IPC
    Magic sleeps added in dd30512 have been removed.
    These magic sleeps were necessary to ensure the last message was sent before stopping the application.
    At this point, for IPC, they are no longer needed since the implementation has been fixed.
    Fix PR: fc4a6358feae
    Note: The magic sleeps were not removed from all tests because they are still needed for boardnet.

--- test/network_tests/restart_routing_tests/restart_routing_test_client.cpp
+++ test/network_tests/restart_routing_tests/restart_routing_test_client.cpp
@@ -19,10 +19,6 @@ void routing_restart_test_client::stop() {
 
     shutdown_service();
 
-    // magic sleep to give time for the last message to be sent
-    // TODO: FIXME! REMOVE THIS!
-    std::this_thread::sleep_for(std::chrono::milliseconds(250));
-
     app_->clear_all_handler();
     app_->stop();
     if (starter_.joinable()) {
