92141c5667701a3480d7e13a7b39a34b7d88a264
danielsantiago <daniel.santiago@ctw.bmwgroup.com>
2026-02-18 17:48:28 +0000

    Fix subscribe_notify_one failures on memcheck
    Add a shutdown handshake mechanism before stopping the service
    The test is failing in memcheck because the service is stopped/sending STOP_OFFER before the client receives and
    processes all notifications.
    This is mitigated by implementing flush_queue() in boardnet, but since it is the service that sends the messages, we
    cannot guarantee processing.
    Given this, a shutdown handshake mechanism was created in which, when the application receives all notifications, it
    sends a shutdown message to all other applications and waits to receive shutdown messages from the other
    applications that were subscribed to it, indicating that they have received all notifications. This way, we ensure that
    applications are only stopped after all clients have received all notifications.

--- test/network_tests/subscribe_notify_one_tests/subscribe_notify_one_test_service.cpp
+++ test/network_tests/subscribe_notify_one_tests/subscribe_notify_one_test_service.cpp
@@ -27,13 +27,13 @@
 class subscribe_notify_one_test_service {
 public:
     subscribe_notify_one_test_service(struct subscribe_notify_one_test::service_info _service_info,
-                                      vsomeip::reliability_type_e _reliability_type) :
+                                      vsomeip::reliability_type_e _reliability_type, bool _is_tcp) :
         service_info_(_service_info), app_(vsomeip::runtime::get()->create_application()), wait_until_registered_(true),
         wait_until_other_services_available_(true), wait_until_notified_from_other_services_(true),
         offer_thread_(std::bind(&subscribe_notify_one_test_service::run, this)), wait_for_stop_(true),
         stop_thread_(std::bind(&subscribe_notify_one_test_service::wait_for_stop, this)), wait_for_notify_(true),
-        notify_thread_(std::bind(&subscribe_notify_one_test_service::notify_one, this)), subscription_state_handler_called_(0),
-        subscription_error_occured_(false), reliability_type_(_reliability_type) {
+        notify_thread_(std::bind(&subscribe_notify_one_test_service::notify_one, this)), wait_for_shutdown_acks_(true),
+        subscription_state_handler_called_(0), subscription_error_occured_(false), reliability_type_(_reliability_type), is_tcp_(_is_tcp) {
         if (!app_->init()) {
             ADD_FAILURE() << "Couldn't initialize application";
             return;
@@ -85,7 +85,12 @@ public:
 
             other_services_available_[std::make_pair(i.service_id, i.instance_id)] = false;
             other_services_received_notification_[std::make_pair(i.service_id, i.method_id)] = 0;
+            other_services_shutdown_ack_[std::make_pair(i.service_id, i.instance_id)] = false;
         }
+
+        // Register handler for shutdown coordination messages on our own service
+        app_->register_message_handler(service_info_.service_id, service_info_.instance_id, 0x0999,
+                                       std::bind(&subscribe_notify_one_test_service::on_shutdown_message, this, std::placeholders::_1));
 
         app_->start();
     }
@@ -188,6 +193,24 @@ public:
         }
     }
 
+    void on_shutdown_message(const std::shared_ptr<vsomeip::message>& _message) {
+        if (_message->get_message_type() == vsomeip::message_type_e::MT_REQUEST) {
+            vsomeip::service_t sender_service = _message->get_client();
+            vsomeip::instance_t sender_instance = _message->get_instance();
+
+            VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] "
+                          << "Received a shutdown coordination message with Client/Session [" << std::setw(4) << service_info_.service_id
+                          << "/" << std::setw(4) << _message->get_session() << "] from Service/Method [" << std::setw(4) << sender_service
+                          << "/" << std::setw(4) << _message->get_method() << "]";
+
+            std::scoped_lock its_lock(shutdown_mutex_);
+            if (other_services_shutdown_ack_[std::make_pair(sender_service, sender_instance)] = true, all_shutdown_acks_received()) {
+                wait_for_shutdown_acks_ = false;
+                shutdown_condition_.notify_one();
+            }
+        }
+    }
+
     void on_message(const std::shared_ptr<vsomeip::message>& _message) {
         if (_message->get_message_type() == vsomeip::message_type_e::MT_NOTIFICATION) {
 
@@ -214,15 +237,45 @@ public:
                            });
     }
 
+    bool all_shutdown_acks_received() {
+        return std::all_of(
+                other_services_shutdown_ack_.cbegin(), other_services_shutdown_ack_.cend(),
+                [&](const std::map<std::pair<vsomeip::service_t, vsomeip::instance_t>, bool>::value_type& v) { return v.second; });
+    }
+
+    void send_shutdown_messages() {
+        for (const auto& i : subscribe_notify_one_test::service_infos) {
+            if ((i.service_id == service_info_.service_id && i.instance_id == service_info_.instance_id)
+                || (i.service_id == 0xFFFF && i.instance_id == 0xFFFF)) {
+                continue;
+            }
+
+            std::shared_ptr<vsomeip::message> its_shutdown_message = vsomeip::runtime::get()->create_request();
+            its_shutdown_message->set_service(i.service_id);
+            its_shutdown_message->set_instance(i.instance_id);
+            its_shutdown_message->set_method(0x0999);
+            its_shutdown_message->set_reliable(is_tcp_);
+
+            VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id
+                          << "] Sending shutdown coordination message to Service/Instance [" << std::setw(4) << i.service_id << "/"
+                          << std::setw(4) << i.instance_id << "]";
+
+            app_->send(its_shutdown_message);
+        }
+    }
+
     void run() {
         VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Running";
         std::unique_lock<std::mutex> its_lock(mutex_);
-        condition_.wait(its_lock, [this] { return !wait_until_registered_; });
+        EXPECT_TRUE(condition_.wait_for(its_lock, std::chrono::seconds(5), [this] { return !wait_until_registered_; }))
+                << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Service registration timeout";
 
         VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Offering";
         offer();
 
-        condition_.wait(its_lock, [this] { return !wait_until_other_services_available_; });
+        EXPECT_TRUE(condition_.wait_for(its_lock, std::chrono::seconds(15), [this] { return !wait_until_other_services_available_; }))
+                << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id
+                << "] Other services availability timeout";
 
         VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Subscribing";
         // subscribe to events of other services
@@ -241,7 +294,8 @@ public:
                           << std::setw(4) << i.eventgroup_id << "]";
         }
 
-        condition_.wait(its_lock, [this] { return !wait_until_notified_from_other_services_; });
+        EXPECT_TRUE(condition_.wait_for(its_lock, std::chrono::seconds(15), [this] { return !wait_until_notified_from_other_services_; }))
+                << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Notification timeout";
 
         // It is possible that we run in the case a subscription is NACKED
         // due to TCP endpoint not completely connected when subscription
@@ -258,7 +312,8 @@ public:
 
     void notify_one() {
         std::unique_lock<std::mutex> its_lock(notify_mutex_);
-        notify_condition_.wait(its_lock, [this] { return !wait_for_notify_; });
+        EXPECT_TRUE(notify_condition_.wait_for(its_lock, std::chrono::seconds(15), [this] { return !wait_for_notify_; }))
+                << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Notify one timeout";
 
         // sleep a while before starting to notify this is necessary as it's not
         // possible to detect if _all_ clients on the remote side have
@@ -287,12 +342,28 @@ public:
 
     void wait_for_stop() {
         std::unique_lock<std::mutex> its_lock(stop_mutex_);
-        stop_condition_.wait(its_lock, [this] { return !wait_for_stop_; });
+        EXPECT_TRUE(stop_condition_.wait_for(its_lock, std::chrono::seconds(15), [this] { return !wait_for_stop_; }))
+                << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Stop timeout";
         VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id
-                      << "] Received notifications from all other services, going down";
+                      << "] Received notifications from all other services, sending shutdown coordination messages";
 
         // wait until all notifications have been sent out
         notify_thread_.join();
+
+        // Send shutdown coordination message to all other services
+        send_shutdown_messages();
+
+        // Wait for shutdown acknowledgments from all other services
+        {
+            std::unique_lock<std::mutex> its_shutdown_lock(shutdown_mutex_);
+            EXPECT_TRUE(
+                    shutdown_condition_.wait_for(its_shutdown_lock, std::chrono::seconds(15), [this] { return !wait_for_shutdown_acks_; }))
+                    << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id
+                    << "] Shutdown coordination timeout";
+        }
+
+        VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id
+                      << "] Received all shutdown acknowledgments, going down";
 
         // let offer thread exit
         {
@@ -326,7 +397,6 @@ public:
             app_->release_event(i.service_id, i.instance_id, i.event_id);
             app_->release_service(i.service_id, i.instance_id);
         }
-        std::this_thread::sleep_for(std::chrono::seconds(1));
         app_->clear_all_handler();
         app_->stop();
     }
@@ -336,6 +406,7 @@ private:
     std::shared_ptr<vsomeip::application> app_;
     std::map<std::pair<vsomeip::service_t, vsomeip::instance_t>, bool> other_services_available_;
     std::map<std::pair<vsomeip::service_t, vsomeip::method_t>, std::uint32_t> other_services_received_notification_;
+    std::map<std::pair<vsomeip::service_t, vsomeip::instance_t>, bool> other_services_shutdown_ack_;
 
     bool wait_until_registered_;
     bool wait_until_other_services_available_;
@@ -354,19 +425,25 @@ private:
     std::condition_variable notify_condition_;
     std::thread notify_thread_;
 
+    bool wait_for_shutdown_acks_;
+    std::mutex shutdown_mutex_;
+    std::condition_variable shutdown_condition_;
+
     std::unordered_set<vsomeip::client_t> subscribers_;
     std::atomic<uint32_t> subscription_state_handler_called_;
     std::atomic<bool> subscription_error_occured_;
 
     std::mutex subscribers_mutex_;
     vsomeip::reliability_type_e reliability_type_;
+    bool is_tcp_;
 };
 
 static unsigned long service_number;
 vsomeip::reliability_type_e reliability_type = vsomeip::reliability_type_e::RT_UNKNOWN;
+static bool is_tcp = false;
 
 TEST(someip_subscribe_notify_one_test, send_ten_notifications_to_service) {
-    subscribe_notify_one_test_service its_sample(subscribe_notify_one_test::service_infos[service_number], reliability_type);
+    subscribe_notify_one_test_service its_sample(subscribe_notify_one_test::service_infos[service_number], reliability_type, is_tcp);
 }
 
 #if defined(__linux__) || defined(__QNX__)
@@ -383,10 +460,13 @@ int main(int argc, char** argv) {
 
     if (std::string("TCP") == std::string(argv[2])) {
         reliability_type = vsomeip::reliability_type_e::RT_RELIABLE;
+        is_tcp = true;
     } else if (std::string("UDP") == std::string(argv[2])) {
         reliability_type = vsomeip::reliability_type_e::RT_UNRELIABLE;
+        is_tcp = false;
     } else if (std::string("TCP_AND_UDP") == std::string(argv[2])) {
         reliability_type = vsomeip::reliability_type_e::RT_BOTH;
+        is_tcp = true;
     }
 
     return test_main(argc, argv, std::chrono::seconds(30));
