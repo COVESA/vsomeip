COVESA/vsomeip

commit 194b674031b7ba6a905c2bcd12afb4d909551bb0

switch to client/server arch
Introduce a client-server architecture:

The endpoint_manager manages endpoints for consuming services (client endpoints),
as well as accepted connections (server endpoints)
The endpoint_manager_impl manages the local_routing_endpoints
(which are a special type of accepted connections)
The router almost always uses the routing_endpoints (because in the former
architecture there might have been two receiving endpoints,
but only a single sending endpoint)

implementation/endpoints/include/endpoint_manager_base.hpp
@@ -9,6 +9,7 @@
 #include <map>
 #include <set>
 #include <unordered_set>
+#include <unordered_map>
 #include <memory>
 #include <optional>
 
@@ -29,18 +30,22 @@ public:
                           const std::shared_ptr<configuration>& _configuration);
     virtual ~endpoint_manager_base() = default;
 
-    std::shared_ptr<local_endpoint> create_local(client_t _client);
-    void remove_local(client_t _client, bool _remove_due_to_error);
+    std::shared_ptr<local_server> create_local_server(const std::shared_ptr<routing_host>& _routing_host);
 
-    std::shared_ptr<local_endpoint> find_or_create_local(client_t _client);
-    std::shared_ptr<local_endpoint> find_local(client_t _client);
-    std::shared_ptr<local_endpoint> find_local(service_t _service, instance_t _instance);
+    std::shared_ptr<local_endpoint> create_local_client(client_t _client);
+    std::shared_ptr<local_endpoint> find_or_create_local_client(client_t _client);
+    std::shared_ptr<local_endpoint> find_local_client(client_t _client);
+    std::shared_ptr<local_endpoint> find_local_client(service_t _service, instance_t _instance);
+
+    // server endpoint API (endpoints towards clients of this application)
+    std::shared_ptr<local_endpoint> find_local_server_endpoint(client_t _client) const;
+
+    void remove_local(client_t _client, bool _remove_due_to_error);
+    void clear_local_endpoints();
 
     void flush_local_endpoint_queues() const;
 
     std::unordered_set<client_t> get_connected_clients() const;
-
-    std::shared_ptr<local_server> create_local_server(const std::shared_ptr<routing_host>& _routing_host);
 
     // Statistics
     void log_client_states() const;
@@ -50,8 +55,14 @@ private:
     client_t get_client_id() const;
     std::string get_client_env() const;
 
-    std::shared_ptr<local_endpoint> create_local_unlocked(client_t _client);
-    std::shared_ptr<local_endpoint> find_local_unlocked(client_t _client);
+    void add_local_server_endpoint(std::shared_ptr<local_endpoint> _connection);
+    void add_local_server_endpoint_unlocked(client_t _client, const std::shared_ptr<local_endpoint>& _connection);
+
+    std::shared_ptr<local_endpoint> create_local_client_unlocked(client_t _client);
+    std::shared_ptr<local_endpoint> find_local_client_unlocked(client_t _client);
+
+    void remove_local_client_endpoint_unlocked(client_t _client, bool _remove_due_to_error);
+    void remove_local_server_endpoint_unlocked(client_t _client, bool _remove_due_to_error);
 
     bool get_local_server_port(port_t& _port, const std::set<port_t>& _used_ports) const;
 
@@ -65,8 +76,18 @@ protected:
                         // vsomeip application via TCP
 
 private:
-    mutable std::mutex local_endpoint_mutex_;
-    std::map<client_t, std::shared_ptr<local_endpoint>> local_endpoints_;
+    mutable std::mutex mtx_;
+    std::map<client_t, std::shared_ptr<local_endpoint>> local_client_endpoints_;
+    std::map<client_t, std::shared_ptr<local_endpoint>> local_server_endpoints_;
+
+    /**
+     * It can happen that a new connection has to be accepted, when a former
+     * endpoint was not cleaned-up. In this situation it is important to first
+     * clean-up all client related state, before accepting a new connection.
+     * (Because one can neither tell whether it is really the same client, nor,
+     * if one would knew, whether all handlers are still installed)
+     **/
+    std::unordered_map<client_t, std::shared_ptr<local_endpoint>> pending_server_endpoints_;
 };
 
 } // namespace vsomeip_v3

commit 2accc4d4a31c232367ca4d70e6f8440291cd6d85

use local_ep for subs from the router
This support broke with 194b674031b7. The problem was the
following:

The router would use the routing endpoint to request
a subscription
The offering client would search for a server endpoint,
assuming that it is a boardnet subscription in case no
endpoint is found
use NOTIFY_ONE instead of SEND_ID (as for the notification
of other local clients)
The router could not understand how to deal with this
message, because there was no boardnet subscription

To circumvent this scenario the pending_subscriptions are send
with the help of (potentially newly) created "client-endpoints"
in the router, and it is checked whether the service is already
available.
With this approach the offering client will find a server endpoint
and should be able to treat this as any other client.
But this approach requires that the client_endpoints of the router,
are not forwarding their messages to the routing_manager_impl.
Which was implictily given for every sender created by the emb.
This did not use to be a problem prior to 194b674031b7,
because the protocol ensured that these clients created by the
router would never receive a message, but only the "receiving endpoints".
But these were created via the created server that would already ensure that the
messages were forwarded to the routing_manager_stub.
Therefore to ensure that the messages are still forwarded to the routing_manager_stub after
194b674031b7, it is now ensured
that every local_endpoint is forwarding the message to the new member
*local_message_handler_" in the emb.
Meanwhile:

To ensure that fake_socket_tests can really test routing subscriptions
the service discovery needs to be enabled.
The new tests failed in different variations, because the port in the
sec_client in the application would not be protected/synchronized.
Therefore the init mutex was reused to ensure synchronization also in
cases where the router would be started and immediately try to
subscribe.

implementation/endpoints/include/endpoint_manager_base.hpp
@@ -30,7 +30,9 @@ public:
                           const std::shared_ptr<configuration>& _configuration);
     virtual ~endpoint_manager_base() = default;
 
-    std::shared_ptr<local_server> create_local_server(const std::shared_ptr<routing_host>& _routing_host);
+    void init(std::shared_ptr<routing_host> const& _local_message_handler);
+
+    std::shared_ptr<local_server> create_local_server();
 
     std::shared_ptr<local_endpoint> create_local_client(client_t _client);
     std::shared_ptr<local_endpoint> find_or_create_local_client(client_t _client);
@@ -77,6 +79,7 @@ protected:
 
 private:
     mutable std::mutex mtx_;
+    std::weak_ptr<routing_host> local_message_handler_;
     std::map<client_t, std::shared_ptr<local_endpoint>> local_client_endpoints_;
     std::map<client_t, std::shared_ptr<local_endpoint>> local_server_endpoints_;
 

implementation/endpoints/src/endpoint_manager_base.cpp
@@ -33,6 +33,11 @@ endpoint_manager_base::endpoint_manager_base(routing_manager_base* const _rm, bo
     is_local_routing_ = configuration_->is_local_routing();
 }
 
+void endpoint_manager_base::init(std::shared_ptr<routing_host> const& _local_message_handler) {
+    std::scoped_lock its_lock(mtx_);
+    local_message_handler_ = _local_message_handler;
+}
+
 std::shared_ptr<local_endpoint> endpoint_manager_base::create_local_client(client_t _client) {
     std::scoped_lock its_lock(mtx_);
     return create_local_client_unlocked(_client);
@@ -134,7 +139,7 @@ std::unordered_set<client_t> endpoint_manager_base::get_connected_clients() cons
     return clients;
 }
 
-std::shared_ptr<local_server> endpoint_manager_base::create_local_server(const std::shared_ptr<routing_host>& _routing_host) {
+std::shared_ptr<local_server> endpoint_manager_base::create_local_server() {
     std::stringstream its_path;
     its_path << utility::get_base_path(configuration_->get_network()) << std::hex << get_client_id();
     const client_t its_client = get_client_id();
@@ -175,12 +180,7 @@ std::shared_ptr<local_server> endpoint_manager_base::create_local_server(const s
                 its_tmp->init(boost::asio::ip::tcp::endpoint(its_address, its_port), its_error);
                 if (!its_error) {
                     VSOMEIP_INFO_P << "Listening @ " << its_address.to_string() << ":" << std::dec << its_port;
-
-                    if (rm_->is_routing_manager()) {
-                        local_port_ = port_t(configuration_->get_routing_host_port() + 1);
-                    } else {
-                        local_port_ = port_t(its_port + 1);
-                    }
+                    local_port_ = port_t(its_port + 1);
                     VSOMEIP_INFO_P << "Connecting to other clients from " << its_address.to_string() << ":" << std::dec << local_port_;
 
                     rm_->set_sec_client_port(local_port_);
@@ -212,7 +212,7 @@ std::shared_ptr<local_server> endpoint_manager_base::create_local_server(const s
 
     if (its_acceptor) {
         return std::make_shared<local_server>(
-                io_, std::move(its_acceptor), configuration_, _routing_host,
+                io_, std::move(its_acceptor), configuration_, local_message_handler_,
                 [weak_self = weak_from_this()](auto _ep) {
                     if (auto self = weak_self.lock(); self) {
                         self->add_local_server_endpoint(std::move(_ep));
@@ -270,7 +270,7 @@ std::shared_ptr<local_endpoint> endpoint_manager_base::create_local_client_unloc
 #if defined(__linux__) || defined(__QNX__)
     if (is_local_routing_) {
         its_endpoint = local_endpoint::create_client_ep(
-                local_endpoint_context{io_, configuration_, rm_->weak_from_this()},
+                local_endpoint_context{io_, configuration_, local_message_handler_},
                 local_endpoint_params{_client, _client == VSOMEIP_ROUTING_CLIENT ? client_t{VSOMEIP_CLIENT_UNSET} : get_client_id(),
                                       std::make_shared<local_socket_uds_impl>(io_, boost::asio::local::stream_protocol::endpoint(""),
                                                                               boost::asio::local::stream_protocol::endpoint(its_path.str()),
@@ -290,7 +290,7 @@ std::shared_ptr<local_endpoint> endpoint_manager_base::create_local_client_unloc
                 its_local_address = configuration_->get_routing_guest_address();
 
                 its_endpoint = local_endpoint::create_client_ep(
-                        local_endpoint_context{io_, configuration_, rm_->weak_from_this()},
+                        local_endpoint_context{io_, configuration_, local_message_handler_},
                         local_endpoint_params{
                                 _client, _client == VSOMEIP_ROUTING_CLIENT ? client_t{VSOMEIP_CLIENT_UNSET} : get_client_id(),
 

implementation/routing/src/routing_manager_client.cpp
@@ -103,6 +103,7 @@ routing_manager_client::~routing_manager_client() { }
 
 void routing_manager_client::init() {
     routing_manager_base::init(std::make_shared<endpoint_manager_base>(this, io_, configuration_));
+    ep_mgr_->init(shared_from_this());
     sender_debounce_ = timer::create(io_, std::chrono::milliseconds(100), [this, weak_self = weak_from_this()] {
         if (auto self = weak_self.lock(); self) {
             debounce_restart_sender_done();
@@ -130,7 +131,7 @@ void routing_manager_client::start() {
         // NOTE: order matters, `create_local_server` must done first
         // with TCP, following `create_local_client` will use whatever port is established there
         if (!configuration_->is_local_routing() && !receiver_) {
-            receiver_ = ep_mgr_->create_local_server(shared_from_this());
+            receiver_ = ep_mgr_->create_local_server();
         }
     }
     std::unique_lock<std::recursive_mutex> lock{sender_mutex_};
@@ -1797,7 +1798,7 @@ void routing_manager_client::reconnect(const std::map<client_t, std::string>& _c
             } else {
                 // TODO this might end up looping forever, if the network is assumed
                 // to be down. How to break out of this loop in case of "stop" ?
-                receiver_ = ep_mgr_->create_local_server(shared_from_this());
+                receiver_ = ep_mgr_->create_local_server();
             }
         } else {
             // But the actual reconnect
@@ -2133,7 +2134,7 @@ bool routing_manager_client::send_pending_commands() {
 
 void routing_manager_client::init_receiver([[maybe_unused]] std::unique_lock<std::mutex> const& _receive_lock) {
     if (!receiver_) {
-        receiver_ = ep_mgr_->create_local_server(shared_from_this());
+        receiver_ = ep_mgr_->create_local_server();
     } else {
         std::uint16_t its_port = receiver_->get_local_port();
         if (its_port != ILLEGAL_PORT)

implementation/routing/src/routing_manager_impl.cpp
@@ -150,6 +150,7 @@ void routing_manager_impl::init() {
 
     if (configuration_->is_routing_enabled()) {
         stub_ = std::make_shared<routing_manager_stub>(this, configuration_);
+        ep_mgr_impl_->init(stub_);
         stub_->init();
     } else {
         VSOMEIP_INFO << "Internal message routing disabled!";
@@ -685,8 +686,13 @@ void routing_manager_impl::subscribe(client_t _client, const vsomeip_sec_client_
 
             if (subscriber_is_rm_host) {
                 subscription_data_t subscription = {_service, _instance, _eventgroup, _major, _event, _filter, *_sec_client};
-                std::scoped_lock ist_lock(pending_subscription_mutex_);
-                pending_subscriptions_.insert(subscription);
+                {
+                    std::scoped_lock ist_lock(pending_subscription_mutex_);
+                    pending_subscriptions_.insert(subscription);
+                }
+                if (is_available(_service, _instance, _major)) {
+                    send_pending_subscriptions(_service, _instance, _major);
+                }
             }
         } else {
             VSOMEIP_ERROR << "SOME/IP eventgroups require SD to be enabled!";
@@ -2996,9 +3002,16 @@ std::shared_ptr<endpoint_manager_impl> routing_manager_impl::get_endpoint_manage
 
 void routing_manager_impl::send_subscribe(client_t _client, service_t _service, instance_t _instance, eventgroup_t _eventgroup,
                                           major_version_t _major, event_t _event, const std::shared_ptr<debounce_filter_impl_t>& _filter) {
-    auto endpoint = find_routing_endpoint(find_local_client(_service, _instance));
+    // this function is only ever called from routing_manager_base for pending subscriptions implying that _client == get_client()
+    // but in order for the offering service to understand that this is not for the boardnet it needs to find a local client
+    // -> create a local client
+    auto const offering_client = find_local_client(_service, _instance);
+    auto endpoint = find_or_create_local_client(offering_client);
     if (endpoint && stub_) {
         stub_->send_subscribe(endpoint, _client, _service, _instance, _eventgroup, _major, _event, _filter, PENDING_SUBSCRIPTION_ID);
+    } else {
+        VSOMEIP_WARNING << "rmi::" << __func__ << ": Could not find/create endpoint for client: 0x" << hex4(offering_client)
+                        << " offering service: " << hex4(_service);
     }
 }
 

implementation/routing/src/routing_manager_stub.cpp
@@ -1368,8 +1368,7 @@ void routing_manager_stub::create_local_receiver() {
         return;
     }
 #endif
-    local_receiver_ =
-            std::static_pointer_cast<endpoint_manager_base>(host_->get_endpoint_manager())->create_local_server(shared_from_this());
+    local_receiver_ = std::static_pointer_cast<endpoint_manager_base>(host_->get_endpoint_manager())->create_local_server();
 
     if (local_receiver_) {
         local_receiver_->set_id(get_client());
@@ -1734,7 +1733,7 @@ bool routing_manager_stub::send_provided_event_resend_request(client_t _client, 
         its_command.serialize(its_buffer, its_error);
 
         if (its_error == protocol::error_e::ERROR_OK) {
-            send_local(its_endpoint, its_buffer);
+            return send_local(its_endpoint, its_buffer);
         }
     } else {
         VSOMEIP_WARNING_P << "Couldn't send provided event resend request to local client: 0x" << hex4(_client);

test/network_tests/fake_socket_tests/conf/multiple_client_one_process.json.in
@@ -55,7 +55,7 @@
     },
     "service-discovery" :
     {
-        "enable": "false"
+        "enable": "true"
     }
 }
 

test/network_tests/fake_socket_tests/helpers/app.cpp
@@ -47,6 +47,11 @@ void app::stop() {
 bool app::is_router() const {
     return app_ && app_->is_routing();
 }
+void app::offer(interface const& _interface) {
+    offer(_interface.instance_);
+    offer_event(_interface.event_one_);
+    offer_field(_interface.field_two_);
+}
 
 void app::offer(service_instance _si) {
     TEST_LOG << "[app] \"" << app_->get_name() << "\" is offering: " << _si;
@@ -61,6 +66,12 @@ void app::offer_event(event_ids const& _ei) {
 void app::offer_field(event_ids const& _ei) {
     TEST_LOG << "[app] \"" << app_->get_name() << "\" is offering: " << _ei;
     offer(_ei, vsomeip::event_type_e::ET_FIELD);
+}
+
+void app::subscribe(interface const& _interface) {
+    request_service(_interface.instance_);
+    subscribe_event(_interface.event_one_);
+    subscribe_field(_interface.field_two_);
 }
 
 void app::subscribe_event(event_ids const& _ei) {
@@ -109,6 +120,10 @@ void app::answer_request(request const& _r, std::function<std::vector<unsigned c
 void app::request_service(service_instance _si) {
     TEST_LOG << "[app] \"" << app_->get_name() << "\" is requesting: " << _si;
     app_->request_service(_si.service_, _si.instance_);
+}
+
+void app::send_field(interface const& _interface, std::vector<unsigned char> const& _payload) {
+    send_event(_interface.field_two_, _payload);
 }
 
 void app::send_event(event_ids const& _ei, std::vector<unsigned char> const& _payload) {

test/network_tests/fake_socket_tests/helpers/app.hpp
@@ -58,6 +58,11 @@ public:
     bool is_router() const;
 
     /**
+     * Forwards the request to offer the contained service, instance, event and field.
+     **/
+    void offer(interface const& _interface);
+
+    /**
      * Forwards the request to the vsomeip::application::offer_service()
      */
     void offer(service_instance _si);
@@ -86,6 +91,11 @@ public:
      * Forwards the request to the vsomeip::application::send()
      */
     void send_request(request const& _req);
+
+    /**
+     * Forwards the request for the service and instance and subscribes to all events and fields that are part of the interface
+     **/
+    void subscribe(interface const& _interface);
 
     /**
      * 1. registers a subscription handler to record subscription events with @see
@@ -154,6 +164,11 @@ public:
      * Forwards the event payload to the vsomeip::application::notify()
      */
     void send_event(event_ids const& _ei, std::vector<unsigned char> const& _payload);
+
+    /**
+     * Forwards the field payload to the vsomeip::application::notify() using the field identifier in the interface
+     */
+    void send_field(interface const& _interface, std::vector<unsigned char> const& _payload);
 
     /**
      * Wait for message (payloads!) to reach given state

test/network_tests/fake_socket_tests/helpers/message_checker.hpp
@@ -1,0 +1,46 @@
+// Copyright (C) 2014-2026 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "service_state.hpp"
+#include <vector>
+#include <optional>
+
+namespace vsomeip_v3::testing {
+
+struct message_checker {
+
+    [[nodiscard]] bool operator==(message const& _message) const {
+        if (client_session_ && client_session_ != _message.client_session_) {
+            return false;
+        }
+        if (service_instance_ && service_instance_ != _message.service_instance_) {
+            return false;
+        }
+        if (method_ && method_ != _message.method_) {
+            return false;
+        }
+        if (message_type_ && message_type_ != _message.message_type_) {
+            return false;
+        }
+        if (payload_ && payload_ != _message.payload_) {
+            return false;
+        }
+        return true;
+    }
+    [[nodiscard]] bool operator!=(message const& _message) const { return !(*this == _message); }
+    [[nodiscard]] bool operator()(std::vector<message> const& _messages) const {
+        return std::any_of(_messages.begin(), _messages.end(), [this](auto const& _rhs) { return *this == _rhs; });
+    }
+
+    std::optional<client_session> client_session_;
+    std::optional<service_instance> service_instance_{};
+    std::optional<vsomeip::method_t> method_{};
+    std::optional<vsomeip::message_type_e> message_type_{};
+
+    std::optional<std::vector<unsigned char>> payload_{};
+};
+}

test/network_tests/fake_socket_tests/helpers/service_state.hpp
@@ -99,6 +99,14 @@ struct service_state {
     [[nodiscard]] bool operator!=(service_state const& rhs) const { return !(*this == rhs); }
 };
 
+struct interface {
+    explicit interface(vsomeip::service_t _service, vsomeip::instance_t _instance = 0x1) : instance_(_service, _instance) { }
+
+    service_instance instance_;
+    event_ids event_one_{instance_, 0x8001, 0x1};
+    event_ids field_two_{instance_, 0x8002, 0x1};
+};
+
 std::ostream& operator<<(std::ostream& o, service_instance const& s);
 std::ostream& operator<<(std::ostream& o, service_availability const& s);
 std::ostream& operator<<(std::ostream& o, client_session const& c);

test/network_tests/fake_socket_tests/sample_interfaces.hpp
@@ -1,0 +1,13 @@
+// Copyright (C) 2014-2026 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "helpers/service_state.hpp"
+
+namespace vsomeip_v3::testing::interfaces {
+static const interface cafe{0xcafe};
+static const interface beef{0xbeef};
+}

test/network_tests/fake_socket_tests/test_connection_restoration.cpp
@@ -3,13 +3,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "helpers/app.hpp"
 #include "helpers/attribute_recorder.hpp"
 #include "helpers/base_fake_socket_fixture.hpp"
-#include "helpers/app.hpp"
 #include "helpers/command_record.hpp"
 #include "helpers/fake_socket_factory.hpp"
 #include "helpers/fake_tcp_socket_handle.hpp"
+#include "helpers/message_checker.hpp"
 #include "helpers/service_state.hpp"
+
+#include "sample_interfaces.hpp"
 
 #include <boost/asio/error.hpp>
 #include <vsomeip/vsomeip.hpp>
@@ -90,6 +93,8 @@ struct test_client_helper : public base_fake_socket_fixture {
     message first_expected_field_message_{client_session{0, 2}, // todo, why is the session a two here?
                                           service_instance_, offered_field_.event_id_, vsomeip::message_type_e::MT_NOTIFICATION,
                                           field_payload_};
+    message_checker const field_checker_{std::nullopt, service_instance_, offered_field_.event_id_,
+                                         vsomeip::message_type_e::MT_NOTIFICATION, field_payload_};
 
     vsomeip_v3::method_t method_{0x1111};
     request request_{service_instance_, method_, vsomeip::message_type_e::MT_REQUEST, {}};
@@ -258,6 +263,103 @@ TEST_F(test_protocol_messages, ensure_sequence_of_request_reply) {
     };
     bool const is_any = expected_sequence1 == *record_ || expected_sequence2 == *record_;
     EXPECT_TRUE(is_any) << *record_;
+}
+TEST_F(test_client_helper, router_consumes_field_before_service_tries_to_offer_field_is_updated_before_registration) {
+    GTEST_SKIP() << "Provokes a race in application_impl regarding the usage of sec_client_";
+
+    start_router();
+    routingmanagerd_->subscribe_field(offered_field_);
+
+    server_ = start_client(server_name_);
+    ASSERT_NE(server_, nullptr);
+    server_->offer(service_instance_);
+    server_->offer_field(offered_field_);
+    send_field_message();
+
+    ASSERT_TRUE(routingmanagerd_->subscription_record_.wait_for_last(event_subscription::successfully_subscribed_to(offered_field_)));
+    EXPECT_TRUE(routingmanagerd_->message_record_.wait_for(field_checker_));
+}
+TEST_F(test_client_helper, router_consumes_field_before_service_tries_to_offer_field_is_updated_after_registration) {
+    GTEST_SKIP() << "Provokes a race in application_impl regarding the usage of sec_client_";
+
+    start_router();
+    routingmanagerd_->subscribe_field(offered_field_);
+
+    server_ = start_client(server_name_);
+    ASSERT_NE(server_, nullptr);
+    server_->offer(service_instance_);
+    server_->offer_field(offered_field_);
+    ASSERT_TRUE(routingmanagerd_->subscription_record_.wait_for_last(event_subscription::successfully_subscribed_to(offered_field_)));
+    send_field_message();
+    EXPECT_TRUE(routingmanagerd_->message_record_.wait_for(field_checker_));
+}
+TEST_F(test_client_helper, router_consumes_field_after_service_tries_to_offer_before_registration) {
+    GTEST_SKIP() << "Provokes a race in application_impl regarding the usage of sec_client_";
+
+    server_ = start_client(server_name_);
+    ASSERT_NE(server_, nullptr);
+    server_->offer(service_instance_);
+    server_->offer_field(offered_field_);
+    send_field_message();
+
+    start_router();
+
+    routingmanagerd_->subscribe_field(offered_field_);
+    ASSERT_TRUE(routingmanagerd_->subscription_record_.wait_for_last(event_subscription::successfully_subscribed_to(offered_field_)));
+    EXPECT_TRUE(routingmanagerd_->message_record_.wait_for(field_checker_));
+}
+TEST_F(test_client_helper, router_consumes_field_after_service_tries_to_offer_after_registration) {
+
+    start_apps();
+    send_field_message();
+
+    // ensure that everything is fully set up...
+    ASSERT_TRUE(subscribe_to_field());
+    ASSERT_TRUE(client_->message_record_.wait_for(field_checker_));
+
+    // ... before subscribing from the router itself
+    routingmanagerd_->subscribe_field(offered_field_);
+    ASSERT_TRUE(routingmanagerd_->subscription_record_.wait_for_last(event_subscription::successfully_subscribed_to(offered_field_)));
+    EXPECT_TRUE(routingmanagerd_->message_record_.wait_for(field_checker_));
+}
+
+TEST_F(test_client_helper, mutual_offerings_and_consumptions_with_router) {
+    // helper structs
+    auto beef_payload = std::vector<unsigned char>{0xf, 0xe, 0xe, 0xd};
+    auto beef_checker = message_checker{std::nullopt, interfaces::beef.instance_, interfaces::beef.field_two_.event_id_,
+                                        vsomeip::message_type_e::MT_NOTIFICATION, beef_payload};
+
+    auto cafe_payload = std::vector<unsigned char>{0xf, 0xa, 0xd, 0xe};
+    auto cafe_checker = message_checker{std::nullopt, interfaces::cafe.instance_, interfaces::cafe.field_two_.event_id_,
+                                        vsomeip::message_type_e::MT_NOTIFICATION, cafe_payload};
+
+    // offering setup
+    start_router();
+    server_ = start_client(server_name_);
+    ASSERT_NE(server_, nullptr);
+    ASSERT_TRUE(server_->app_state_record_.wait_for_last(vsomeip::state_type_e::ST_REGISTERED));
+
+    // only offer now, otherwise the routing_manager will encounter
+    // data race in the sec_client usage :/
+    routingmanagerd_->offer(interfaces::beef);
+    routingmanagerd_->send_field(interfaces::beef, beef_payload);
+    server_->offer(interfaces::cafe);
+    server_->send_field(interfaces::cafe, cafe_payload);
+
+    // ensure setup is fully operational
+    client_ = start_client(client_name_);
+    ASSERT_NE(client_, nullptr);
+    ASSERT_TRUE(client_->app_state_record_.wait_for_last(vsomeip::state_type_e::ST_REGISTERED));
+    client_->subscribe(interfaces::cafe);
+    client_->subscribe(interfaces::beef);
+    ASSERT_TRUE(client_->message_record_.wait_for(cafe_checker));
+    ASSERT_TRUE(client_->message_record_.wait_for(beef_checker));
+
+    // do the actual subscription and ensure subscription is working
+    routingmanagerd_->subscribe(interfaces::cafe);
+    server_->subscribe(interfaces::beef);
+    EXPECT_TRUE(routingmanagerd_->message_record_.wait_for(cafe_checker));
+    EXPECT_TRUE(server_->message_record_.wait_for(beef_checker));
 }
 
 TEST_F(test_client_helper, ensure_unavail_after_stop_offer) {