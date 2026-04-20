b393711c61c891eb752d3af9f1743c28698f96f2
thalessouza <thales.souza@ctw.bmwgroup.com>
2026-04-20 09:48:06 +0100

    Add arguments to timeout log
    Add arguments to timeout_detector's log.
    Send the entire argv to timeout_detector, allowing us to see exactly which process was aborted.
    The string is built on construction to avoid doing any "unnecessary" steps during signal handlers.
    This is specially useful for tests with several applications.

--- test/common/include/common/timeout_detector.hpp
+++ test/common/include/common/timeout_detector.hpp
@@ -9,24 +9,45 @@
 #include <csignal>
 #include <cstring>
 #include <cstdlib>
+#include <cstddef>
+#include <string>
+#include <unistd.h>
+#include <thread>
 
 class timeout_detector {
 public:
-    timeout_detector(uint32_t _timeout = 180) {
+    timeout_detector(int _argc, char** _argv, uint32_t _timeout = 180) {
+        command_ = "";
+        build_log(_argc, _argv);
         signal(SIGALRM, alarmHandler);
         (void)!alarm(_timeout);
     }
 
 private:
-    static void alarmHandler(int signal) {
-        if (signal == SIGALRM) {
-            // https://stackoverflow.com/questions/16891019/how-to-avoid-using-printf-in-a-signal-handler
-            // can hardly do any logging, but it is important to show what the cause of failure is
-            // so use a raw write to stdout/stderr (best-effort, write might not succeed...)
-            const char* str = "timeout_detector blew up! THE TEST WILL SIGABRT\n";
-            (void)!write(STDOUT_FILENO, str, strlen(str));
+    inline static std::string command_{};
 
+    static void alarmHandler(int _signal) {
+        const char* str = command_.c_str();
+        size_t len = command_.length();
+        if (_signal == SIGALRM) {
+            (void)!write(STDOUT_FILENO, str, len);
             std::abort();
         }
+    }
+
+    void build_log(int argc, char** argv) {
+        std::string prefix = "timeout_detector blew up for '";
+        std::string null_arg = "<null>";
+        std::string suffix = "'! PROCESS " + std::to_string(getpid()) + " WILL SIGABRT\n";
+
+        command_ = prefix;
+        for (int i = 0; i < argc; i++) {
+            if (!argv[i][0])
+                command_ += null_arg;
+            else
+                command_ += argv[i];
+            command_ += ' ';
+        }
+        command_ += suffix;
     }
 };
