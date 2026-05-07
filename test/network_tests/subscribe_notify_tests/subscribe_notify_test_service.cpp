b2e935b7e4511cb5f3de10429a1e0de30e623dec
duartefonseca <duarte.fonseca@ctw.bmwgroup.com>
2026-05-07 12:16:22 +0100

    add sync mechanism for subscribe_notify_tests
    remove a 1 second sleep, add a sync mechanism
    The clients on subscribe_notify_test previously waited for 1000 ms before starting to send notifications.
    This was a workaround for the fact that a single remote subscription
    message does not reveal how many individual clients on the remote node had subscribed.
    Now every client sends a message to every other client after receiving all the expected acks.
    After every ack and request is received in all clients, the test notifications are sent.

--- test/network_tests/subscribe_notify_tests/subscribe_notify_test_service.cpp
+++ test/network_tests/subscribe_notify_tests/subscribe_notify_test_service.cpp
@@ -27,13 +27,13 @@ class subscribe_notify_test_service {
 public:
     subscribe_notify_test_service(struct subscribe_notify_test::service_info _service_info,
                                   std::array<subscribe_notify_test::service_info, 7> _service_infos,
-                                  vsomeip::reliability_type_e _reliability_type) :
+                                  vsomeip::reliability_type_e _reliability_type, bool _use_tcp) :
         service_info_(_service_info), service_infos_(_service_infos), app_(vsomeip::runtime::get()->create_application()),
         wait_until_registered_(true), wait_until_other_services_available_(true), wait_until_notified_from_other_services_(true),
         offer_thread_(std::bind(&subscribe_notify_test_service::run, this)), wait_for_stop_(true),
-        stop_thread_(std::bind(&subscribe_notify_test_service::wait_for_stop, this)), wait_for_notify_(true),
-        notify_thread_(std::bind(&subscribe_notify_test_service::notify, this)), subscription_error_occured_(false),
-        reliability_type_(_reliability_type) {
+        stop_thread_(std::bind(&subscribe_notify_test_service::wait_for_stop, this)), all_requests_received_(false),
+        notify_thread_(std::bind(&subscribe_notify_test_service::notify, this)), expected_requests_(0), requests_received_(0),
+        subscription_error_occured_(false), reliability_type_(_reliability_type), use_tcp_(_use_tcp) {
         if (!app_->init()) {
             ADD_FAILURE() << "Couldn't initialize application";
             return;
@@ -73,6 +73,7 @@ public:
 
             other_services_available_[std::make_pair(i.service_id, i.instance_id)] = false;
             other_services_received_notification_[std::make_pair(i.service_id, i.method_id)] = 0;
+            ++expected_requests_;
         }
 
         // register subscription handler to detect whether or not all other
@@ -113,7 +114,7 @@ public:
             if (its_service != other_services_available_.end()) {
                 if (its_service->second != _is_available) {
                     its_service->second = true;
-                    VSOMEIP_INFO << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Service ["
+                    VSOMEIP_INFO << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << ": Service ["
                                  << std::setw(4) << _service << "." << _instance << "] is available.";
                 }
             }
@@ -130,15 +131,40 @@ public:
 
     void on_subscription_state_change(const vsomeip::service_t _service, const vsomeip::instance_t _instance,
                                       const vsomeip::eventgroup_t _eventgroup, const vsomeip::event_t _event, const uint16_t _error) {
-        (void)_event;
+        vsomeip::method_t request_method = vsomeip::ANY_METHOD;
 
         std::scoped_lock its_lock(subscription_state_handler_called_mutex_);
         if (!_error) {
+            if ((_service == service_info_.service_id && _instance == service_info_.instance_id)
+                || (_service == 0xFFFF && _instance == 0xFFFF)) {
+                return;
+            }
+
+            const auto peer_it = std::find_if(service_infos_.begin(), service_infos_.end(),
+                                              [=](const auto& s) { return s.service_id == _service && s.instance_id == _instance; });
+            if (peer_it == service_infos_.end()) {
+                return;
+            }
+
             subscription_state_handler_called_.insert(std::make_tuple(_service, _instance, _eventgroup));
+
+            request_method = peer_it->method_id;
+
+            VSOMEIP_INFO << "[" << std::hex << service_info_.service_id << "] subscription state OK for [" << _service << "." << _instance
+                         << "." << _eventgroup << "." << _event << "]";
         } else {
             subscription_error_occured_ = true;
-            VSOMEIP_WARNING << std::hex << app_->get_client() << " : on_subscription_state_change: for service " << std::hex << _service
-                            << " received a subscription error!";
+            VSOMEIP_WARNING << std::hex << service_info_.service_id << " : on_subscription_state_change: for service " << std::hex
+                            << _service << "." << _instance << "." << _eventgroup << "." << _event << " received a subscription error!";
+        }
+
+        // Trigger request when subscription ACK is received.
+        if (request_method != vsomeip::ANY_METHOD) {
+            std::shared_ptr<vsomeip::message> its_req = vsomeip::runtime::get()->create_request(use_tcp_);
+            its_req->set_service(_service);
+            its_req->set_instance(_instance);
+            its_req->set_method(request_method);
+            app_->send(its_req);
         }
     }
 
@@ -146,7 +172,6 @@ public:
         (void)_uid;
         (void)_gid;
         std::scoped_lock its_lock(subscribers_mutex_);
-        static bool notified(false);
         if (_subscribed) {
             subscribers_.insert(_client);
         } else {
@@ -156,16 +181,6 @@ public:
         VSOMEIP_DEBUG << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] "
                       << "Client: " << std::setw(4) << _client << (_subscribed ? " subscribed" : " unsubscribed") << ", now have "
                       << std::dec << subscribers_.size() << " subscribers";
-        // check if all other services have subscribed:
-        // half the services subscribe remote -> + 1 (because the router will subscribe as a placeholder)
-        // while the other half - 1 subscribe locally
-        if (!notified && subscribers_.size() == (service_infos_.size() / 2)) {
-            // notify the notify thread to start sending out notifications
-            std::scoped_lock its_lock(notify_mutex_);
-            wait_for_notify_ = false;
-            notify_condition_.notify_one();
-            notified = true;
-        }
 
         return true;
     }
@@ -174,8 +189,15 @@ public:
         if (_message->get_message_type() == vsomeip::message_type_e::MT_REQUEST) {
             VSOMEIP_DEBUG << "Received a request with Client/Session [" << std::hex << std::setfill('0') << std::setw(4)
                           << _message->get_client() << "/" << std::setw(4) << _message->get_session() << "]";
-            std::shared_ptr<vsomeip::message> its_response = vsomeip::runtime::get()->create_response(_message);
-            app_->send(its_response);
+            ++requests_received_;
+
+            if (requests_received_ == expected_requests_) {
+                std::scoped_lock its_lock(notify_mutex_);
+                all_requests_received_ = true;
+                notify_condition_.notify_one();
+            }
+            VSOMEIP_INFO << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << std::dec
+                         << "] Received requests: " << requests_received_ << "/" << expected_requests_;
         }
     }
 
@@ -249,15 +271,10 @@ public:
     }
 
     void notify() {
+        // Requests are sent after subscription acks are received.
+        // Wait until all peers have requested before sending notifications.
         std::unique_lock its_lock(notify_mutex_);
-        notify_condition_.wait(its_lock, [this] { return !wait_for_notify_; });
-
-        // sleep a while before starting to notify this is necessary as it's not
-        // possible to detect if _all_ clients on the remote side have
-        // successfully subscribed as we only receive once subscription per
-        // remote node no matter how many clients subscribed to this eventgroup
-        // on the remote node
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        notify_condition_.wait(its_lock, [this] { return all_requests_received_; });
 
         VSOMEIP_INFO << "[" << std::hex << std::setfill('0') << std::setw(4) << service_info_.service_id << "] Starting to notify";
 
@@ -342,10 +359,12 @@ private:
     std::thread stop_thread_;
 
     std::set<vsomeip::client_t> subscribers_;
-    bool wait_for_notify_;
+    bool all_requests_received_;
     std::mutex notify_mutex_;
     std::condition_variable notify_condition_;
     std::thread notify_thread_;
+    std::uint32_t expected_requests_;
+    std::atomic<std::uint32_t> requests_received_;
 
     std::mutex subscription_state_handler_called_mutex_;
     bool subscription_error_occured_;
@@ -353,19 +372,22 @@ private:
 
     std::mutex subscribers_mutex_;
     vsomeip::reliability_type_e reliability_type_;
+    bool use_tcp_;
 };
 
 static unsigned long service_number;
 static bool use_same_service_id;
+static bool use_tcp = false;
+
 vsomeip::reliability_type_e reliability_type = vsomeip::reliability_type_e::RT_UNKNOWN;
 
 TEST(someip_subscribe_notify_test, send_ten_notifications_to_service) {
     if (use_same_service_id) {
         subscribe_notify_test_service its_sample(subscribe_notify_test::service_infos_same_service_id[service_number],
-                                                 subscribe_notify_test::service_infos_same_service_id, reliability_type);
+                                                 subscribe_notify_test::service_infos_same_service_id, reliability_type, use_tcp);
     } else {
         subscribe_notify_test_service its_sample(subscribe_notify_test::service_infos[service_number], subscribe_notify_test::service_infos,
-                                                 reliability_type);
+                                                 reliability_type, use_tcp);
     }
 }
 
@@ -387,9 +409,12 @@ int main(int argc, char** argv) {
     if (argc >= 3) {
         if (std::string("TCP") == std::string(argv[2])) {
             reliability_type = vsomeip::reliability_type_e::RT_RELIABLE;
+            use_tcp = true;
         } else if (std::string("UDP") == std::string(argv[2])) {
             reliability_type = vsomeip::reliability_type_e::RT_UNRELIABLE;
+            use_tcp = false;
         } else if (std::string("TCP_AND_UDP") == std::string(argv[2])) {
+            use_tcp = true;
             reliability_type = vsomeip::reliability_type_e::RT_BOTH;
         }
     }
