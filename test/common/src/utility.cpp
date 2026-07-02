911a001d06600255898b25304f6adf549c0dd0d7
Joerg-Christian Boehme <joerg@chaosdorf.de>
2026-07-02 23:16:43 +0200

    Update utility.cpp add boost version header

--- test/common/src/utility.cpp
+++ test/common/src/utility.cpp
@@ -3,6 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include <boost/version.hpp>
 #include <common/utility.hpp>
 
 void utility::load_policy_data(std::string _input, std::vector<vsomeip_v3::configuration_element>& _elements,
